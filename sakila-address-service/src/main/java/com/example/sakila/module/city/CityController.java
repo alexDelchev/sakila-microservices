55770caa535cde1ab818c4d32dd23cc8b7955d74
alexDelchev <a.r.delchev@outlook.com>
2019-11-24 00:26:14 +0200

    Add City entity to dto converstion method

--- sakila-address-service/src/main/java/com/example/sakila/module/city/CityController.java
+++ sakila-address-service/src/main/java/com/example/sakila/module/city/CityController.java
@@ -1,8 +1,13 @@
 package com.example.sakila.module.city;
 
 import com.example.sakila.generated.server.api.CityApi;
+import com.example.sakila.generated.server.model.CityDTO;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
 
 @Controller
 public class CityController implements CityApi {
@@ -12,5 +17,13 @@ public class CityController implements CityApi {
   @Autowired
   public CityController(CityService cityService) {
     this.cityService = cityService;
+  }
+
+  private CityDTO toDTO(City city) {
+    CityDTO result = new CityDTO();
+    BeanUtils.copyProperties(result, city);
+    result.setCountryId(city.getCountry().getId());
+    result.setLastUpdate(OffsetDateTime.ofInstant(city.getLastUpdate().toInstant(), ZoneId.systemDefault()));
+    return result;
   }
 }
