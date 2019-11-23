379060839f482676fdffedbd7e7fcf71aa2ec3a8
alexDelchev <a.r.delchev@outlook.com>
2019-11-23 23:55:17 +0200

    Add City entity

---
+++ sakila-address-service/src/main/java/com/example/sakila/module/city/City.java
@@ -0,0 +1,72 @@
+package com.example.sakila.module.city;
+
+import com.example.sakila.module.country.Country;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "city")
+public class City {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "city_id")
+  private Long id;
+
+  @Column(name = "city")
+  private String city;
+
+  @ManyToOne(cascade = CascadeType.ALL)
+  @JoinColumn(name = "country_id")
+  private Country country;
+
+  @Column(name = "last_update")
+  @Temporal(TemporalType.TIMESTAMP)
+  private Date lastUpdate;
+
+  City() {}
+
+  City(Long id) {
+    this.id = id;
+  }
+
+  City(Long id, String city, Country country, Date lastUpdate) {
+    this.id = id;
+    this.city = city;
+    this.country = country;
+    this.lastUpdate = lastUpdate;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getCity() {
+    return city;
+  }
+
+  public void setCity(String city) {
+    this.city = city;
+  }
+
+  public Country getCountry() {
+    return country;
+  }
+
+  public void setCountry(Country country) {
+    this.country = country;
+  }
+
+  public Date getLastUpdate() {
+    return lastUpdate;
+  }
+
+  public void setLastUpdate(Date lastUpdate) {
+    this.lastUpdate = lastUpdate;
+  }
+}
