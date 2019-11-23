9f51861a15860828e0c6da501d3ff2afbd0cfeec
alexDelchev <a.r.delchev@outlook.com>
2019-11-23 23:54:02 +0200

    Add CityRepository interface

---
+++ sakila-address-service/src/main/java/com/example/sakila/module/city/repository/CityRepository.java
@@ -0,0 +1,16 @@
+package com.example.sakila.module.city.repository;
+
+import com.example.sakila.module.city.City;
+
+import java.util.List;
+
+public interface CityRepository {
+
+  City getCityById(Long id);
+
+  City getCityByAddressId(Long addressId);
+
+  List<City> getCitiesByCountry(Long cityId);
+
+  List<City> getAllCities();
+}
