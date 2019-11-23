8c8932dcc421b40e3f5ded13ccf20740f3321010
alexDelchev <a.r.delchev@outlook.com>
2019-11-23 23:16:43 +0200

    Add AddressRepository hibernate implementation

---
+++ sakila-address-service/src/main/java/com/example/sakila/module/address/repository/AddressRepositoryHibernateImpl.java
@@ -0,0 +1,41 @@
+package com.example.sakila.module.address.repository;
+
+import com.example.sakila.module.address.Address;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Repository
+public class AddressRepositoryHibernateImpl implements AddressRepository {
+
+  @PersistenceContext
+  private EntityManager entityManager;
+
+  @Override
+  public Address getAddressById(Long id) {
+    return entityManager.find(Address.class, id);
+  }
+
+  @Override
+  public List<Address> getAddressesByCity(Long cityId) {
+    TypedQuery<Address> query = createQuery("SELECT a FROM Address a WHERE city_id = :cityId");
+    query.setParameter("cityId", cityId);
+    return query.getResultList();
+  }
+
+  @Override
+  public List<Address> getAddressesByCountry(Long countryId) {
+    TypedQuery<Address> query = createQuery("SELECT a FROM Address a WHERE country_id = :countryId");
+    query.setParameter("countryId", countryId);
+    return query.getResultList();
+  }
+
+  private TypedQuery<Address> createQuery(String query) {
+    return entityManager.createQuery(query, Address.class);
+  }
+
+
+}
