e3531484eae5699347ab551e9ab2284cfbcb0275
alexDelchev <a.r.delchev@outlook.com>
2019-11-25 00:45:18 +0200

    Implement 'city' endpoint

--- sakila-address-service/src/main/java/com/example/sakila/module/city/CityController.java
+++ sakila-address-service/src/main/java/com/example/sakila/module/city/CityController.java
@@ -2,12 +2,14 @@ package com.example.sakila.module.city;
 
 import com.example.sakila.generated.server.api.CityApi;
 import com.example.sakila.generated.server.model.CityDTO;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
 import java.time.OffsetDateTime;
 import java.time.ZoneId;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class CityController implements CityApi {
@@ -19,9 +21,17 @@ public class CityController implements CityApi {
     this.cityService = cityService;
   }
 
+  @Override
+  public ResponseEntity<List<CityDTO>> getAllCities() {
+    return ResponseEntity.ok(
+        cityService.getAllCities().stream().map(this::toDTO).collect(Collectors.toList())
+    );
+  }
+
   private CityDTO toDTO(City city) {
     CityDTO result = new CityDTO();
-    BeanUtils.copyProperties(result, city);
+    result.setId(city.getId());
+    result.setCity(city.getCity());
     result.setCountryId(city.getCountry().getId());
     result.setLastUpdate(OffsetDateTime.ofInstant(city.getLastUpdate().toInstant(), ZoneId.systemDefault()));
     return result;
