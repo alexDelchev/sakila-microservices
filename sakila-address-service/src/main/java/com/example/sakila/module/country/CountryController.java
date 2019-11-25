f8ef813807ea3af5ae3c13f3be8a22d271728c69
alexDelchev <a.r.delchev@outlook.com>
2019-11-25 22:19:00 +0200

    Remove unused import

--- sakila-address-service/src/main/java/com/example/sakila/module/country/CountryController.java
+++ sakila-address-service/src/main/java/com/example/sakila/module/country/CountryController.java
@@ -4,7 +4,6 @@ import com.example.sakila.generated.server.api.CountriesApi;
 import com.example.sakila.generated.server.model.CountryDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.OffsetDateTime;
