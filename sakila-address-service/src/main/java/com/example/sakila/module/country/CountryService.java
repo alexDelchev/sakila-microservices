599e591c770b28b930f6083f365c6cae53ec3ea3
alexDelchev <a.r.delchev@outlook.com>
2019-11-25 22:10:30 +0200

    Add CountryService

---
+++ sakila-address-service/src/main/java/com/example/sakila/module/country/CountryService.java
@@ -0,0 +1,32 @@
+package com.example.sakila.module.country;
+
+import com.example.sakila.module.country.repository.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CountryService {
+
+  private final CountryRepository countryRepository;
+
+  @Autowired
+  private CountryService(CountryRepository countryRepository) {
+    this.countryRepository = countryRepository;
+  }
+
+  public Country getCountryById(Long id) {
+    if (id == null) return null;
+    return countryRepository.getCountryById(id);
+  }
+
+  public Country getCountryByAddressId(Long addressId) {
+    if (addressId == null) return null;
+    return countryRepository.getCountryByAddressId(addressId);
+  }
+
+  public List<Country> getAllCountries() {
+    return countryRepository.getAllCountries();
+  }
+}
