934ecb473f8b24e414e04391038b5a93c17a244e
alexDelchev <a.r.delchev@outlook.com>
2019-11-27 23:06:12 +0200

    Add Category entity

---
+++ sakila-film-service/src/main/java/com/example/sakila/module/category/Category.java
@@ -0,0 +1,57 @@
+package com.example.sakila.module.category;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "category")
+public class Category {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "category_id")
+  private Long id;
+
+  @Column(name = "name")
+  private String name;
+
+  @Column(name = "last_update")
+  @Temporal(TemporalType.TIMESTAMP)
+  private Date lastUpdate;
+
+  Category() {}
+
+  Category(Long id) {
+    this.id = id;
+  }
+
+  Category(Long id, String name, Date lastUpdate) {
+    this.id = id;
+    this.name = name;
+    this.lastUpdate = lastUpdate;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Date getLastUpdate() {
+    return lastUpdate;
+  }
+
+  public void setLastUpdate(Date lastUpdate) {
+    this.lastUpdate = lastUpdate;
+  }
+}
