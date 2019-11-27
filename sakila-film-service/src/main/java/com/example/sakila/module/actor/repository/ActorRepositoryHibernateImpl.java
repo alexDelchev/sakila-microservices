a6cb774aff26d0abf4930e2c7acb5b70546a1e7f
alexDelchev <a.r.delchev@outlook.com>
2019-11-27 20:26:03 +0200

    Add ActoRepository hibernate implementation

---
+++ sakila-film-service/src/main/java/com/example/sakila/module/actor/repository/ActorRepositoryHibernateImpl.java
@@ -0,0 +1,32 @@
+package com.example.sakila.module.actor.repository;
+
+import com.example.sakila.module.actor.Actor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Repository
+public class ActorRepositoryHibernateImpl implements ActorRepository {
+
+  @PersistenceContext
+  private EntityManager entityManager;
+
+  @Override
+  public Actor getActorById(Long id) {
+    return entityManager.find(Actor.class, id);
+  }
+
+  @Override
+  public List<Actor> getActorsByFilm(Long filmId) {
+    TypedQuery<Actor> query = createQuery("SELECT a FROM Actor a INNER JOIN actor_film af WHERE af.film_id = :filmId");
+    query.setParameter("filmId", filmId);
+    return query.getResultList();
+  }
+
+  private TypedQuery<Actor> createQuery(String query) {
+    return entityManager.createQuery(query, Actor.class);
+  }
+}
