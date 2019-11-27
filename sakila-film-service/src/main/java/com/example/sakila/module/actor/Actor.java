25b3ccd3a7bc2c0b7be8869fff1f2d31b1e80891
alexDelchev <a.r.delchev@outlook.com>
2019-11-27 20:25:06 +0200

    Add Actor entity

---
+++ sakila-film-service/src/main/java/com/example/sakila/module/actor/Actor.java
@@ -0,0 +1,69 @@
+package com.example.sakila.module.actor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "actor")
+public class Actor {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "actor_id")
+  private Long id;
+
+  @Column(name = "first_name")
+  private String firstName;
+
+  @Column(name = "last_name")
+  private String lastName;
+
+  @Column(name = "last_update")
+  @Temporal(TemporalType.TIMESTAMP)
+  private Date lastUpdate;
+
+  Actor() {}
+
+  Actor(Long id) {
+    this.id = id;
+  }
+
+  Actor(Long id, String firstName, String lastName, Date lastUpdate) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.lastUpdate = lastUpdate;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public Date getLastUpdate() {
+    return lastUpdate;
+  }
+
+  public void setLastUpdate(Date lastUpdated) {
+    this.lastUpdate = lastUpdated;
+  }
+}
