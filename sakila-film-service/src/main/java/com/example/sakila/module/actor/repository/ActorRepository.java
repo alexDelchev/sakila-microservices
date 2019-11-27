bf3b41d5b49afa4079c5eb86259a3ed97fc55a84
alexDelchev <a.r.delchev@outlook.com>
2019-11-27 20:24:06 +0200

    Add ActorRepository interface

---
+++ sakila-film-service/src/main/java/com/example/sakila/module/actor/repository/ActorRepository.java
@@ -0,0 +1,12 @@
+package com.example.sakila.module.actor.repository;
+
+import com.example.sakila.module.actor.Actor;
+
+import java.util.List;
+
+public interface ActorRepository {
+
+  Actor getActorById(Long id);
+
+  List<Actor> getActorsByFilm(Long filmId);
+}
