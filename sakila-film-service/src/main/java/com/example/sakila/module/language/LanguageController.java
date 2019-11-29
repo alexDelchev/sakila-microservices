d02caeed9ac0c5ca9d94bfb199554212f2c531df
alexDelchev <a.r.delchev@outlook.com>
2019-11-29 13:43:48 +0200

    Implement getAllLanguages operation

--- sakila-film-service/src/main/java/com/example/sakila/module/language/LanguageController.java
+++ sakila-film-service/src/main/java/com/example/sakila/module/language/LanguageController.java
@@ -1,15 +1,38 @@
 package com.example.sakila.module.language;
 
+import com.example.sakila.generated.server.api.LanguagesApi;
+import com.example.sakila.generated.server.model.LanguageDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Controller
-public class LanguageController {
+public class LanguageController implements LanguagesApi {
 
   private final LanguageService languageService;
 
   @Autowired
   public LanguageController(LanguageService languageService) {
     this.languageService = languageService;
+  }
+
+  @Override
+  public ResponseEntity<List<LanguageDTO>> getAllLanguages() {
+    return ResponseEntity.ok(
+        languageService.getAllLanguages().stream().map(this::toDTO).collect(Collectors.toList())
+    );
+  }
+
+  private LanguageDTO toDTO(Language language) {
+    LanguageDTO languageDTO = new LanguageDTO();
+    languageDTO.setId(language.getId());
+    languageDTO.setName(language.getName());
+    languageDTO.setLastUpdate(OffsetDateTime.ofInstant(language.getLastUpdate().toInstant(), ZoneId.systemDefault()));
+    return languageDTO;
   }
 }
