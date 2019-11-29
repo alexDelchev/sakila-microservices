abc1c77cacce8a61c796761accf2ede95e340500
alexDelchev <a.r.delchev@outlook.com>
2019-11-29 13:38:01 +0200

    Remove unnecessary import

--- sakila-film-service/src/main/java/com/example/sakila/module/language/LanguageController.java
+++ sakila-film-service/src/main/java/com/example/sakila/module/language/LanguageController.java
@@ -1,6 +1,5 @@
 package com.example.sakila.module.language;
 
-import com.example.sakila.module.language.LanguageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
