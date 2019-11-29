e67a56dd7364a2dfc20f7bc05d84074e5eba8c40
alexDelchev <a.r.delchev@outlook.com>
2019-11-29 13:36:43 +0200

    Add LanguageController

---
+++ sakila-film-service/src/main/java/com/example/sakila/module/language/repository/LanguageController.java
@@ -0,0 +1,16 @@
+package com.example.sakila.module.language.repository;
+
+import com.example.sakila.module.language.LanguageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class LanguageController {
+
+  private final LanguageService languageService;
+
+  @Autowired
+  public LanguageController(LanguageService languageService) {
+    this.languageService = languageService;
+  }
+}
