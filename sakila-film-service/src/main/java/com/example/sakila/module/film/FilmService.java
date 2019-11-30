73ab63f0d6bd7fd08eb896cf170270e6a9190a13
alexDelchev <a.r.delchev@outlook.com>
2019-11-30 11:57:31 +0200

    Add FilmService

---
+++ sakila-film-service/src/main/java/com/example/sakila/module/film/FilmService.java
@@ -0,0 +1,45 @@
+package com.example.sakila.module.film;
+
+import com.example.sakila.module.film.repository.FilmRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Service
+public class FilmService {
+
+  private final FilmRepository filmRepository;
+
+  @Autowired
+  public FilmService(FilmRepository filmRepository) {
+    this.filmRepository = filmRepository;
+  }
+
+  public Film getFilmId(Long id) {
+    if (id == null) return null;
+    return filmRepository.getFilmById(id);
+  }
+
+  public List<Film> searchFilmsByTitle(String searchExpression) {
+    if (searchExpression == null) return null;
+    return filmRepository.searchFilmsByTitle(searchExpression);
+  }
+
+  public List<Film> searchFilmsByDescription(String searchExpression) {
+    if (searchExpression == null) return null;
+    return filmRepository.searchFilmsByDescription(searchExpression);
+  }
+
+  public List<Film> getFilmsByCategoryId(Long categoryId) {
+    if (categoryId == null) return null;
+    return filmRepository.getFilmsByCategory(categoryId);
+  }
+
+  public List<Film> getFilmsByLanguageId(Long languageId) {
+    if (languageId == null) return null;
+    return filmRepository.getFilmsByLanguage(languageId);
+  }
+}
