312f75ee2254f92c73356df89560812ef2dd1c27
alexDelchev <a.r.delchev@outlook.com>
2019-11-30 11:56:39 +0200

    Add FilmRepository interface

---
+++ sakila-film-service/src/main/java/com/example/sakila/module/film/repository/FilmRepository.java
@@ -0,0 +1,19 @@
+package com.example.sakila.module.film.repository;
+
+import com.example.sakila.module.film.Film;
+
+import java.util.List;
+
+public interface FilmRepository {
+
+  Film getFilmById(Long id);
+
+  List<Film> searchFilmsByTitle(String searchExpression);
+
+  List<Film> searchFilmsByDescription(String searchExpression);
+
+  List<Film> getFilmsByCategory(Long categoryId);
+
+  List<Film> getFilmsByLanguage(Long languageId);
+
+}
