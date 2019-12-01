c50b14832bed1d9c1982a8fa2b32eab7ae47588a
alexDelchev <a.r.delchev@outlook.com>
2019-12-01 21:03:56 +0200

    Add PaymentRepository hibernate implementation

---
+++ sakila-payment-service/src/main/java/com/example/sakila/module/payment/repository/PaymentRepositoryHibernateImpl.java
@@ -0,0 +1,46 @@
+package com.example.sakila.module.payment.repository;
+
+import com.example.sakila.module.payment.Payment;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Repository
+public class PaymentRepositoryHibernateImpl implements PaymentRepository {
+
+  @PersistenceContext
+  private EntityManager entityManager;
+
+  @Override
+  public Payment getPaymentById(Long id) {
+    return entityManager.find(Payment.class, id);
+  }
+
+  @Override
+  public Payment getPaymentByRentalId(Long id) {
+    TypedQuery<Payment> query = createQuery("SELECT p FROM Payment p WHERE p.rental.id = :rentalId");
+    query.setParameter("rentalId", id);
+    return query.getSingleResult();
+  }
+
+  @Override
+  public List<Payment> getPaymentsByCustomerId(Long id) {
+    TypedQuery<Payment> query = createQuery("SELECT p FROM Payment p WHERE p.customer.id = :customerId");
+    query.setParameter("customerId", id);
+    return query.getResultList();
+  }
+
+  @Override
+  public List<Payment> getPaymentsByStaffId(Long id) {
+    TypedQuery<Payment> query = createQuery("SELECT p FROM Payment p WHERE p.staff_id = :staffId");
+    query.setParameter("staffId", id);
+    return query.getResultList();
+  }
+
+  private TypedQuery<Payment> createQuery(String sql) {
+    return entityManager.createQuery(sql, Payment.class);
+  }
+}
