af81baa59a0424394b38c511e445782308abd5f8
alexDelchev <a.r.delchev@outlook.com>
2019-12-01 13:58:15 +0200

    Add Customer entity

---
+++ sakila-payment-service/src/main/java/com/example/sakila/module/customer/Customer.java
@@ -0,0 +1,145 @@
+package com.example.sakila.module.customer;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "customer")
+public class Customer {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "customer_id")
+  private Long id;
+
+  @Column(name = "store_id")
+  private Long store_id;
+
+  @Column(name = "first_name")
+  private String firstName;
+
+  @Column(name = "last_name")
+  private String lastName;
+
+  @Column(name = "email")
+  private String email;
+
+  @Column(name = "address_id")
+  private Long address_id;
+
+  @Column(name = "activebool")
+  private Boolean activeBool;
+
+  @Column(name = "create_date")
+  @Temporal(TemporalType.DATE)
+  private Date createDate;
+
+  @Column(name = "last_update")
+  @Temporal(TemporalType.TIMESTAMP)
+  private Date lastUpdate;
+
+  @Column(name = "active")
+  private Integer active;
+
+  Customer() {}
+
+  Customer(Long id) {
+    this.id = id;
+  }
+
+  Customer(
+      Long id, Long store_id, String firstName, String lastName, String email,
+      Long address_id, Boolean activeBool, Date createDate, Date lastUpdate, Integer active
+  ) {
+    this.id = id;
+    this.store_id = store_id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.address_id = address_id;
+    this.activeBool = activeBool;
+    this.createDate = createDate;
+    this.lastUpdate = lastUpdate;
+    this.active = active;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Long getStore_id() {
+    return store_id;
+  }
+
+  public void setStore_id(Long store_id) {
+    this.store_id = store_id;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public Long getAddress_id() {
+    return address_id;
+  }
+
+  public void setAddress_id(Long address_id) {
+    this.address_id = address_id;
+  }
+
+  public Boolean getActiveBool() {
+    return activeBool;
+  }
+
+  public void setActiveBool(Boolean activeBool) {
+    this.activeBool = activeBool;
+  }
+
+  public Date getCreateDate() {
+    return createDate;
+  }
+
+  public void setCreateDate(Date createDate) {
+    this.createDate = createDate;
+  }
+
+  public Date getLastUpdate() {
+    return lastUpdate;
+  }
+
+  public void setLastUpdate(Date lastUpdate) {
+    this.lastUpdate = lastUpdate;
+  }
+
+  public Integer getActive() {
+    return active;
+  }
+
+  public void setActive(Integer active) {
+    this.active = active;
+  }
+}
