5e0ca6636c0c13b24929769bce300b478e37a681
alexDelchev <a.r.delchev@outlook.com>
2019-12-01 13:58:41 +0200

    Add CustomerRepository interface

---
+++ sakila-payment-service/src/main/java/com/example/sakila/module/customer/repository/CustomerRepository.java
@@ -0,0 +1,17 @@
+package com.example.sakila.module.customer.repository;
+
+import com.example.sakila.module.customer.Customer;
+
+import java.util.List;
+
+public interface CustomerRepository {
+
+  Customer getCustomerById(Long id);
+
+  List<Customer> getCustomersByStoreId(Long id);
+
+  List<Customer> searchCustomersByFirstName(String firstName);
+
+  List<Customer> searchCustomersByLastName(String lastName);
+
+}
