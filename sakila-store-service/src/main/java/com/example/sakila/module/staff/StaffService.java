cc2cf02a3a7244bf5a0f1c97023e2222782ca791
alexDelchev <a.r.delchev@outlook.com>
2019-12-02 11:27:30 +0200

    Add StaffService

---
+++ sakila-store-service/src/main/java/com/example/sakila/module/staff/StaffService.java
@@ -0,0 +1,28 @@
+package com.example.sakila.module.staff;
+
+import com.example.sakila.module.staff.repository.StaffRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StaffService {
+
+  private final StaffRepository staffRepository;
+
+  @Autowired
+  public StaffService(StaffRepository staffRepository) {
+    this.staffRepository = staffRepository;
+  }
+
+  public Staff getStaffById(Long id) {
+    if (id == null) return null;
+    return staffRepository.getStaffById(id);
+  }
+
+  public List<Staff> getStaffByStoreId(Long storeId) {
+    if (storeId == null) return null;
+    return staffRepository.getStaffByStoreId(storeId);
+  }
+}
