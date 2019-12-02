492fa89b5c2a2590bd21de00fe4739b1a51b3d63
alexDelchev <a.r.delchev@outlook.com>
2019-12-02 11:26:23 +0200

    Add Staff entity

---
+++ sakila-store-service/src/main/java/com/example/sakila/module/staff/Staff.java
@@ -0,0 +1,148 @@
+package com.example.sakila.module.staff;
+
+import com.example.sakila.module.store.Store;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "staff")
+public class Staff {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "staff_id")
+  private Long id;
+
+  @Column(name = "first_name")
+  private String firstName;
+
+  @Column(name = "last_name")
+  private String lastName;
+
+  @Column(name = "address_id")
+  private Long address_id;
+
+  @Column(name = "email")
+  private String email;
+
+  @ManyToOne(cascade = CascadeType.ALL)
+  @JoinColumn(name = "store_id")
+  private Store store;
+
+  @Column(name = "active")
+  private Boolean active;
+
+  @Column(name = "username")
+  private String userName;
+
+  @Column(name = "password")
+  private String password;
+
+  @Column(name = "last_update")
+  @Temporal(TemporalType.TIMESTAMP)
+  private Date lastUpdate;
+
+  Staff() {}
+
+  Staff(Long id) {
+    this.id = id;
+  }
+
+  Staff(
+      Long id, String firstName, String lastName, Long address_id,
+      String email, Store store, Boolean active, String userName,
+      String password, Date lastUpdate
+  ) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.address_id = address_id;
+    this.email = email;
+    this.store = store;
+    this.active = active;
+    this.userName = userName;
+    this.password = password;
+    this.lastUpdate = lastUpdate;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public Long getAddress_id() {
+    return address_id;
+  }
+
+  public void setAddress_id(Long address_id) {
+    this.address_id = address_id;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public Store getStore() {
+    return store;
+  }
+
+  public void setStore(Store store) {
+    this.store = store;
+  }
+
+  public Boolean getActive() {
+    return active;
+  }
+
+  public void setActive(Boolean active) {
+    this.active = active;
+  }
+
+  public String getUserName() {
+    return userName;
+  }
+
+  public void setUserName(String userName) {
+    this.userName = userName;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public Date getLastUpdate() {
+    return lastUpdate;
+  }
+
+  public void setLastUpdate(Date lastUpdate) {
+    this.lastUpdate = lastUpdate;
+  }
+}
