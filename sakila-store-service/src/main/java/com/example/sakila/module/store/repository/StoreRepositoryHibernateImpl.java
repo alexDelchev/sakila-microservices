a16b0e95e0fcc79e17d6e6a9cd17819d4b7bd2c2
alexDelchev <a.r.delchev@outlook.com>
2019-12-02 11:50:18 +0200

    Add StoreRepository hibernate implementation

---
+++ sakila-store-service/src/main/java/com/example/sakila/module/store/repository/StoreRepositoryHibernateImpl.java
@@ -0,0 +1,31 @@
+package com.example.sakila.module.store.repository;
+
+import com.example.sakila.module.store.Store;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+@Repository
+public class StoreRepositoryHibernateImpl implements StoreRepository {
+
+  @PersistenceContext
+  private EntityManager entityManager;
+
+  @Override
+  public Store getStoreById(Long id) {
+    return entityManager.find(Store.class, id);
+  }
+
+
+  public Store getStoreByAddressId(Long addressId) {
+    TypedQuery<Store> query = createQuery("SELECT s FROM Store s WHERE s.address_id = :addressId");
+    query.setParameter("addressId", addressId);
+    return query.getSingleResult();
+  }
+
+  private TypedQuery<Store> createQuery(String query) {
+    return entityManager.createQuery(query, Store.class);
+  }
+}
