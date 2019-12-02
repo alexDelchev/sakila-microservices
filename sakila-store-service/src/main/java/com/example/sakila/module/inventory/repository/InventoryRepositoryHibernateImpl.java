cb274befaba6cfa43992c15b5c20558d609441fa
alexDelchev <a.r.delchev@outlook.com>
2019-12-02 10:54:06 +0200

    Add InventoryRepository hibernate implementation

---
+++ sakila-store-service/src/main/java/com/example/sakila/module/inventory/repository/InventoryRepositoryHibernateImpl.java
@@ -0,0 +1,40 @@
+package com.example.sakila.module.inventory.repository;
+
+import com.example.sakila.module.inventory.Inventory;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Repository
+public class InventoryRepositoryHibernateImpl implements InventoryRepository {
+
+  @PersistenceContext
+  private EntityManager entityManager;
+
+
+  @Override
+  public Inventory getInventoryById(Long id) {
+    return entityManager.find(Inventory.class, id);
+  }
+
+  @Override
+  public List<Inventory> getInventoriesByStore(Long storeId) {
+    TypedQuery<Inventory> query = createQuery("SELECT i FROM Inventory i WHERE store_id = :storeId");
+    query.setParameter("storeId", storeId);
+    return query.getResultList();
+  }
+
+  @Override
+  public List<Inventory> getInventoriesByFilm(Long filmId) {
+    TypedQuery<Inventory> query = createQuery("SELET i FROM Inventory WHERE film_id = :filmId");
+    query.setParameter("filmId", filmId);
+    return query.getResultList();
+  }
+
+  private TypedQuery<Inventory> createQuery(String query) {
+    return entityManager.createQuery(query, Inventory.class);
+  }
+}
