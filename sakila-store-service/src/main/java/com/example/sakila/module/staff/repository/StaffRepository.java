3848b2443442f0c93843ef5ad2174d9c232417e6
alexDelchev <a.r.delchev@outlook.com>
2019-12-02 11:26:46 +0200

    Add StaffRepository interface

---
+++ sakila-store-service/src/main/java/com/example/sakila/module/staff/repository/StaffRepository.java
@@ -0,0 +1,12 @@
+package com.example.sakila.module.staff.repository;
+
+import com.example.sakila.module.staff.Staff;
+
+import java.util.List;
+
+public interface StaffRepository {
+
+  Staff getStaffById(Long id);
+
+  List<Staff> getStaffByStoreId(Long id);
+}
