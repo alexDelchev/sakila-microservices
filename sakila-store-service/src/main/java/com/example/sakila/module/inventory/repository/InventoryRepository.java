b025c3193bdfd6722a83ccd701a1722e7d9a4472
alexDelchev <a.r.delchev@outlook.com>
2019-12-02 10:53:42 +0200

    Add InventoryRepository interface

---
+++ sakila-store-service/src/main/java/com/example/sakila/module/inventory/repository/InventoryRepository.java
@@ -0,0 +1,15 @@
+package com.example.sakila.module.inventory.repository;
+
+import com.example.sakila.module.inventory.Inventory;
+
+import java.util.List;
+
+public interface InventoryRepository {
+
+  Inventory getInventoryById(Long id);
+
+  List<Inventory> getInventoriesByStore(Long storeId);
+
+  List<Inventory> getInventoriesByFilm(Long filmId);
+
+}
