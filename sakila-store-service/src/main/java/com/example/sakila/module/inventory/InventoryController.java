02545f418b2d3c5f800ea2a1bb17c760488fd576
alexDelchev <a.r.delchev@outlook.com>
2019-12-02 11:04:12 +0200

    Implement getInventoryById api operation

--- sakila-store-service/src/main/java/com/example/sakila/module/inventory/InventoryController.java
+++ sakila-store-service/src/main/java/com/example/sakila/module/inventory/InventoryController.java
@@ -1,16 +1,37 @@
 package com.example.sakila.module.inventory;
 
+import com.example.sakila.generated.server.api.InventoriesApi;
+import com.example.sakila.generated.server.model.InventoryDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
 
 @Controller
-public class InventoryController {
+public class InventoryController implements InventoriesApi {
 
   private final InventoryService inventoryService;
 
   @Autowired
   public InventoryController(InventoryService inventoryService) {
     this.inventoryService = inventoryService;
+  }
+
+  @Override
+  public ResponseEntity<InventoryDTO> getInventoryById(@PathVariable("id") Long id) {
+    return ResponseEntity.ok(toDTO(inventoryService.getInventoryById(id)));
+  }
+
+  private InventoryDTO toDTO(Inventory inventory) {
+    InventoryDTO inventoryDTO = new InventoryDTO();
+    inventoryDTO.setId(inventory.getId());
+    inventoryDTO.setFilmId(inventory.getFilm_id());
+    inventoryDTO.setStoreId(inventory.getStore().getId());
+    inventoryDTO.setLastUpdate(OffsetDateTime.ofInstant(inventory.getLastUpdate().toInstant(), ZoneId.systemDefault()));
+    return inventoryDTO;
   }
 
 }
