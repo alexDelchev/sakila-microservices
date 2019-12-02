3fe811f65f22699709fa1a475d7fd3140a65eb94
alexDelchev <a.r.delchev@outlook.com>
2019-12-03 00:14:18 +0200

    Throw NotFoundExcpetion when no results are found

--- sakila-address-service/src/main/java/com/example/sakila/module/address/AddressController.java
+++ sakila-address-service/src/main/java/com/example/sakila/module/address/AddressController.java
@@ -1,5 +1,7 @@
 package com.example.sakila.module.address;
 
+import com.example.sakila.exception.BadRequestException;
+import com.example.sakila.exception.NotFoundException;
 import com.example.sakila.generated.server.api.AddressesApi;
 import com.example.sakila.generated.server.model.AddressDTO;
 import org.springframework.beans.BeanUtils;
@@ -25,21 +27,30 @@ public class AddressController implements AddressesApi {
 
   @Override
   public ResponseEntity<AddressDTO> getAddressById(@PathVariable("id") Long id) {
-    return ResponseEntity.ok(toDTO(addressService.getAddressById(id)));
+    Address address = addressService.getAddressById(id);
+    if (address == null) throw new NotFoundException("Address with ID " + id + " does not exist");
+
+    return ResponseEntity.ok(toDTO(address));
   }
 
   @Override
   public ResponseEntity<List<AddressDTO>> getAddressesByCityId(@PathVariable("id") Long id) {
-    return ResponseEntity.ok(
-        addressService.getAddressesByCity(id).stream().map(this::toDTO).collect(Collectors.toList())
+    List<Address> addresses = addressService.getAddressesByCity(id);
+    if (addresses == null || addresses.size() == 0) throw new NotFoundException(
+        "Addresses for City ID " + id + " do not exist"
     );
+
+    return ResponseEntity.ok(addresses.stream().map(this::toDTO).collect(Collectors.toList()));
   }
 
   @Override
   public ResponseEntity<List<AddressDTO>> getAddressesByCountryId(@PathVariable("id") Long id) {
-    return ResponseEntity.ok(
-        addressService.getAddressesByCountry(id).stream().map(this::toDTO).collect(Collectors.toList())
+    List<Address> addresses = addressService.getAddressesByCountry(id);
+    if (addresses == null || addresses.size() == 0) throw new NotFoundException(
+        "Addresses for Country ID " + id + " do not exist"
     );
+
+    return ResponseEntity.ok(addresses.stream().map(this::toDTO).collect(Collectors.toList()));
   }
 
   private AddressDTO toDTO(Address address) {
