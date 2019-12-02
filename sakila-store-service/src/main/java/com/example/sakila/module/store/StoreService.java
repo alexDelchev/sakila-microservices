2065ea53125033f4e5c8143d6f671c26bfdee29a
alexDelchev <a.r.delchev@outlook.com>
2019-12-02 11:50:40 +0200

    Add StoreService

---
+++ sakila-store-service/src/main/java/com/example/sakila/module/store/StoreService.java
@@ -0,0 +1,26 @@
+package com.example.sakila.module.store;
+
+import com.example.sakila.module.store.repository.StoreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StoreService {
+
+  private final StoreRepository storeRepository;
+
+  @Autowired
+  public StoreService(StoreRepository storeRepository) {
+    this.storeRepository = storeRepository;
+  }
+
+  Store getStoreById(Long id) {
+    if (id == null) return null;
+    return storeRepository.getStoreById(id);
+  }
+
+  Store getStoreByIaddressId(Long addressId) {
+    if (addressId == null) return null;
+    return storeRepository.getStoreByAddressId(addressId);
+  }
+}
