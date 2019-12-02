babbcf2c8ab889e2bc4698e59311b9ae07402778
alexDelchev <a.r.delchev@outlook.com>
2019-12-02 10:54:32 +0200

    Add InventoryService

---
+++ sakila-store-service/src/main/java/com/example/sakila/module/inventory/InventoryService.java
@@ -0,0 +1,33 @@
+package com.example.sakila.module.inventory;
+
+import com.example.sakila.module.inventory.repository.InventoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class InventoryService {
+
+  private final InventoryRepository inventoryRepository;
+
+  @Autowired
+  public InventoryService(InventoryRepository inventoryRepository) {
+    this.inventoryRepository = inventoryRepository;
+  }
+
+  public Inventory getInventoryById(Long id) {
+    if (id == null) return null;
+    return inventoryRepository.getInventoryById(id);
+  }
+
+  public List<Inventory> getInventoriesByStore(Long storeId) {
+    if (storeId == null) return null;
+    return inventoryRepository.getInventoriesByStore(storeId);
+  }
+
+  public List<Inventory> getInventoriesByFilm(Long filmId) {
+    if (filmId == null) return null;
+    return inventoryRepository.getInventoriesByFilm(filmId);
+  }
+}
