89204d5a7b60ae218e7786ea3eab09b65636abc2
alexDelchev <a.r.delchev@outlook.com>
2019-12-02 11:59:59 +0200

    Implement getStoreById api operation

--- sakila-store-service/src/main/java/com/example/sakila/module/store/StoreController.java
+++ sakila-store-service/src/main/java/com/example/sakila/module/store/StoreController.java
@@ -1,10 +1,17 @@
 package com.example.sakila.module.store;
 
+import com.example.sakila.generated.server.api.StoresApi;
+import com.example.sakila.generated.server.model.StoreDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
 
 @Controller
-public class StoreController {
+public class StoreController implements StoresApi {
 
   private final StoreService storeService;
 
@@ -13,4 +20,17 @@ public class StoreController {
     this.storeService = storeService;
   }
 
+  @Override
+  public ResponseEntity<StoreDTO> getStoreById(@PathVariable("id") Long id) {
+    return ResponseEntity.ok(toDTO(storeService.getStoreById(id)));
+  }
+
+  private StoreDTO toDTO(Store store) {
+    StoreDTO storeDTO = new StoreDTO();
+    storeDTO.setId(store.getId());
+    storeDTO.setManagerStaffId(store.getManagerStaff().getId());
+    storeDTO.setAddressId(store.getAddress_id());
+    storeDTO.setLastUpdate(OffsetDateTime.ofInstant(store.getLastUpdate().toInstant(), ZoneId.systemDefault()));
+    return storeDTO;
+  }
 }
