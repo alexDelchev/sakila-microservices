53838a8f988b876eb7e608f8829c2853838a6428
alexDelchev <a.r.delchev@outlook.com>
2019-12-09 23:46:04 +0200

    Add getAddressById test

--- sakila-address-service/src/test/java/com/example/sakila/module/address/AddressServiceTest.java
+++ sakila-address-service/src/test/java/com/example/sakila/module/address/AddressServiceTest.java
@@ -1,10 +1,13 @@
 package com.example.sakila.module.address;
 
 import com.example.sakila.module.address.repository.AddressRepository;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class AddressServiceTest {
@@ -15,4 +18,10 @@ class AddressServiceTest {
   @InjectMocks
   private AddressService addressService;
 
+  @Test
+  void getAddressById() {
+    Address address = addressService.getAddressById(null);
+
+    assertNull(address);
+  }
 }
