9330d1ec7a141f87ac10c00542e66052d3796fa1
alexDelchev <a.r.delchev@outlook.com>
2019-12-12 00:34:38 +0200

    Implement addNewCity api spec

--- sakila-address-service/src/main/java/com/example/sakila/module/city/CityController.java
+++ sakila-address-service/src/main/java/com/example/sakila/module/city/CityController.java
@@ -3,13 +3,16 @@ package com.example.sakila.module.city;
 import com.example.sakila.exception.NotFoundException;
 import com.example.sakila.generated.server.api.CitiesApi;
 import com.example.sakila.generated.server.model.CityDTO;
+import com.example.sakila.module.country.CountryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.time.OffsetDateTime;
 import java.time.ZoneId;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -18,9 +21,12 @@ public class CityController implements CitiesApi {
 
   private final CityService cityService;
 
+  private final CountryService countryService;
+
   @Autowired
-  public CityController(CityService cityService) {
+  public CityController(CityService cityService, CountryService countryService) {
     this.cityService = cityService;
+    this.countryService = countryService;
   }
 
   @Override
@@ -48,6 +54,11 @@ public class CityController implements CitiesApi {
     return ResponseEntity.ok(toDTO(city));
   }
 
+  @Override
+  public ResponseEntity<CityDTO> addNewCity(@RequestBody CityDTO cityDTO) {
+    return ResponseEntity.ok(toDTO(cityService.addNewCity(toEntity(cityDTO))));
+  }
+
   private CityDTO toDTO(City city) {
     CityDTO result = new CityDTO();
     result.setId(city.getId());
@@ -55,5 +66,14 @@ public class CityController implements CitiesApi {
     result.setCountryId(city.getCountry().getId());
     result.setLastUpdate(OffsetDateTime.ofInstant(city.getLastUpdate().toInstant(), ZoneId.systemDefault()));
     return result;
+  }
+
+  private City toEntity(CityDTO cityDTO) {
+    City city = new City();
+    city.setId(cityDTO.getId());
+    city.setCity(cityDTO.getCity());
+    city.setCountry(countryService.getCountryById(cityDTO.getCountryId()));
+    if (cityDTO.getLastUpdate() != null) city.setLastUpdate(Date.from(cityDTO.getLastUpdate().toInstant()));
+    return city;
   }
 }
