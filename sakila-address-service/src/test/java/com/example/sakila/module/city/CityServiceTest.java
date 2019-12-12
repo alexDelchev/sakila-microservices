3a3f10ebf973f96c506aaacbf641d2f6f6f4f0b5
alexDelchev <a.r.delchev@outlook.com>
2019-12-12 20:11:04 +0200

    Add updateCity test

--- sakila-address-service/src/test/java/com/example/sakila/module/city/CityServiceTest.java
+++ sakila-address-service/src/test/java/com/example/sakila/module/city/CityServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.sakila.module.city;
 
+import com.example.sakila.exception.NotFoundException;
 import com.example.sakila.module.city.repository.CityRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,6 +11,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class CityServiceTest {
@@ -39,5 +42,19 @@ class CityServiceTest {
     List<City> cities = cityService.getCitiesByCountry(null);
 
     assertNull(cities);
+  }
+
+  @Test
+  void updateCity() {
+    final long EXISTING_CITY_ID = 1L;
+    when(cityRepository.getCityById(EXISTING_CITY_ID)).thenReturn(new City());
+
+    final long NON_EXISTING_CITY_ID = -1L;
+    when(cityRepository.getCityById(NON_EXISTING_CITY_ID)).thenReturn(null);
+
+    when(cityRepository.updateCity(any(City.class))).thenReturn(new City());
+
+    assertDoesNotThrow(() -> cityService.updateCity(EXISTING_CITY_ID, new City()));
+    assertThrows(NotFoundException.class, () -> cityService.updateCity(NON_EXISTING_CITY_ID, new City()));
   }
 }
