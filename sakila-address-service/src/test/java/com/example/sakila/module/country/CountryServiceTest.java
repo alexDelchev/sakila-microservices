3b867db8af3ee2c307ba919618d40afdfe502b7e
alexDelchev <a.r.delchev@outlook.com>
2019-12-13 00:14:15 +0200

    Add CountryServiceTest

---
+++ sakila-address-service/src/test/java/com/example/sakila/module/country/CountryServiceTest.java
@@ -0,0 +1,20 @@
+package com.example.sakila.module.country;
+
+import com.example.sakila.module.country.repository.CountryRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class CountryServiceTest {
+
+  @Mock
+  private CountryRepository countryRepository;
+
+  @InjectMocks
+  private CountryService countryService;
+  
+}
