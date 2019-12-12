2fdb1ed562f83c2f5457aa3d815bcfdfa5403f34
alexDelchev <a.r.delchev@outlook.com>
2019-12-12 20:16:26 +0200

    Add addNewCity test

--- sakila-address-service/src/test/java/com/example/sakila/module/city/CityControllerTest.java
+++ sakila-address-service/src/test/java/com/example/sakila/module/city/CityControllerTest.java
@@ -1,6 +1,9 @@
 package com.example.sakila.module.city;
 
+import com.example.sakila.exception.NotFoundException;
+import com.example.sakila.generated.server.model.CityDTO;
 import com.example.sakila.module.country.CountryService;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -19,5 +22,13 @@ class CityControllerTest {
 
   @InjectMocks
   private CityController cityController;
-  
+
+  @Test
+  void addNewCity() {
+    final long NON_EXISTING_COUNTRY_ID = -1L;
+    CityDTO invalidDTO = new CityDTO();
+    invalidDTO.setCountryId(NON_EXISTING_COUNTRY_ID);
+
+    assertThrows(NotFoundException.class, () -> cityController.addNewCity(invalidDTO));
+  }
 }
