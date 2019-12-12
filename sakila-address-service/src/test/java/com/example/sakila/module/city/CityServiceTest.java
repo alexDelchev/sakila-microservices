738bee4a8277f5bd0d49631e70bad5d826defc2e
alexDelchev <a.r.delchev@outlook.com>
2019-12-12 20:05:48 +0200

    Add getCityById, getCityByAddressId, betCitiesByCountryId tests

--- sakila-address-service/src/test/java/com/example/sakila/module/city/CityServiceTest.java
+++ sakila-address-service/src/test/java/com/example/sakila/module/city/CityServiceTest.java
@@ -1,10 +1,13 @@
 package com.example.sakila.module.city;
 
 import com.example.sakila.module.city.repository.CityRepository;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,5 +19,25 @@ class CityServiceTest {
 
   @InjectMocks
   private CityService cityService;
-  
+
+  @Test
+  void getCityById() {
+    City city = cityService.getCityById(null);
+
+    assertNull(city);
+  }
+
+  @Test
+  void getCityByAddressId() {
+    City city = cityService.getCityByAddressId(null);
+
+    assertNull(city);
+  }
+
+  @Test
+  void getCitiesByCountryId() {
+    List<City> cities = cityService.getCitiesByCountry(null);
+
+    assertNull(cities);
+  }
 }
