0862a555345b919d57108ca59541db2cac1ef68a
alexDelchev <a.r.delchev@outlook.com>
2019-12-14 00:44:28 +0200

    Add updateActor test

--- sakila-film-service/src/test/java/com/example/sakila/module/actor/ActorServiceTest.java
+++ sakila-film-service/src/test/java/com/example/sakila/module/actor/ActorServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.sakila.module.actor;
 
+import com.example.sakila.exception.NotFoundException;
 import com.example.sakila.module.actor.repository.ActorRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,6 +11,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class ActorServiceTest {
@@ -32,5 +35,19 @@ class ActorServiceTest {
     List<Actor> actors = actorService.getActorsByFilmId(null);
 
     assertNull(actors);
+  }
+
+  @Test
+  void updateActor() {
+    final long EXISTING_ACTOR_ID = 1L;
+    when(actorRepository.getActorById(EXISTING_ACTOR_ID)).thenReturn(new Actor());
+
+    final long NON_EXISTING_ACTOR_ID = -1L;
+    when(actorRepository.getActorById(NON_EXISTING_ACTOR_ID)).thenReturn(null);
+
+    when(actorRepository.updateActor(any(Actor.class))).thenReturn(new Actor());
+
+    assertDoesNotThrow(() -> actorService.updateActor(EXISTING_ACTOR_ID, new Actor()));
+    assertThrows(NotFoundException.class, () -> actorService.updateActor(NON_EXISTING_ACTOR_ID, new Actor()));
   }
 }
