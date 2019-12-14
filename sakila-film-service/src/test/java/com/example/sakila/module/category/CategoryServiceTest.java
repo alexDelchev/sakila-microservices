1d5bd5713a0188156d6ecb1059ba06c799db3706
alexDelchev <a.r.delchev@outlook.com>
2019-12-14 23:22:46 +0200

    Add updateCategory test

--- sakila-film-service/src/test/java/com/example/sakila/module/category/CategoryServiceTest.java
+++ sakila-film-service/src/test/java/com/example/sakila/module/category/CategoryServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.sakila.module.category;
 
+import com.example.sakila.exception.NotFoundException;
 import com.example.sakila.module.category.repository.CategoryRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -8,6 +9,8 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class CategoryServiceTest {
@@ -30,5 +33,22 @@ class CategoryServiceTest {
     Category category = categoryService.getCategoryByFilmId(null);
 
     assertNull(category);
+  }
+
+  @Test
+  void updateCategory() {
+    final long EXISTING_CATEGORY_ID = 1L;
+    when(categoryRepository.getCategoryById(EXISTING_CATEGORY_ID)).thenReturn(new Category());
+
+    final long NON_EXISTING_CATEGORY_ID = -1L;
+    when(categoryRepository.getCategoryById(NON_EXISTING_CATEGORY_ID)).thenReturn(null);
+
+    when(categoryRepository.updateCategory(any(Category.class))).thenReturn(new Category());
+
+    assertDoesNotThrow(() -> categoryService.updateCategory(EXISTING_CATEGORY_ID, new Category()));
+    assertThrows(
+        NotFoundException.class,
+        () -> categoryService.updateCategory(NON_EXISTING_CATEGORY_ID, new Category())
+    );
   }
 }
