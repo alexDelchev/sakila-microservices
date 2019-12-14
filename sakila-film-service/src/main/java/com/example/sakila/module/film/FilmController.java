f06e3729816f7b1a55912943a0ba6179704822f2
alexDelchev <a.r.delchev@outlook.com>
2019-12-15 01:34:23 +0200

    Implement createFilm api operation

--- sakila-film-service/src/main/java/com/example/sakila/module/film/FilmController.java
+++ sakila-film-service/src/main/java/com/example/sakila/module/film/FilmController.java
@@ -1,13 +1,20 @@
 package com.example.sakila.module.film;
 
+import com.example.sakila.exception.NotFoundException;
 import com.example.sakila.generated.server.api.FilmsApi;
 import com.example.sakila.generated.server.model.FilmDTO;
 import com.example.sakila.generated.server.model.FilmRating;
+import com.example.sakila.module.category.Category;
+import com.example.sakila.module.category.CategoryService;
+import com.example.sakila.module.language.Language;
+import com.example.sakila.module.language.LanguageService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.sql.Date;
 import java.time.OffsetDateTime;
 import java.time.ZoneId;
 import java.util.Arrays;
@@ -19,8 +26,14 @@ public class FilmController implements FilmsApi {
 
   private final FilmService filmService;
 
-  public FilmController(FilmService filmService ){
+  private final CategoryService categoryService;
+
+  private final LanguageService languageService;
+
+  public FilmController(FilmService filmService, CategoryService categoryService, LanguageService languageService){
     this.filmService = filmService;
+    this.categoryService = categoryService;
+    this.languageService = languageService;
   }
 
   @Override
@@ -68,6 +81,11 @@ public class FilmController implements FilmsApi {
     );
   }
 
+  @Override
+  public ResponseEntity<FilmDTO> createFilm(@RequestBody FilmDTO filmDTO) {
+    return ResponseEntity.ok(toDTO(filmService.createFilm(toEntity(filmDTO))));
+  }
+
   private FilmDTO toDTO(Film film) {
     FilmDTO filmDTO = new FilmDTO();
     filmDTO.setId(film.getId());
@@ -85,5 +103,42 @@ public class FilmController implements FilmsApi {
     if (film.getSpecialFeatures() != null) filmDTO.setSpecialFeatures(Arrays.asList(film.getSpecialFeatures()));
     filmDTO.setLastUpdate(OffsetDateTime.ofInstant(film.getLastUpdate().toInstant(), ZoneId.systemDefault()));
     return filmDTO;
+  }
+
+  private Film toEntity(FilmDTO filmDTO) {
+    Film film = new Film();
+    film.setId(filmDTO.getId());
+    film.setTitle(filmDTO.getTitle());
+    film.setDescription(filmDTO.getDescription());
+    film.setReleaseYear(filmDTO.getReleaseYear());
+    if (filmDTO.getLanguageId() != null) film.setLanguage(getLanguage(filmDTO.getLanguageId()));
+    if (filmDTO.getOriginalLanguageId() != null) film.setOriginalLanguage(getLanguage(filmDTO.getOriginalLanguageId()));
+    film.setRentalDuration(filmDTO.getRentalDuration());
+    film.setRentalRate(filmDTO.getRentalRate());
+    film.setLength(filmDTO.getLength());
+    film.setReplacementCost(filmDTO.getReplacementCost());
+    if (filmDTO.getRating() != null) film.setRating(filmDTO.getRating().toString());
+    if (filmDTO.getCategoryId() != null) film.setCategory(getCategory(filmDTO.getCategoryId()));
+    if (filmDTO.getSpecialFeatures() != null) {
+      film.setSpecialFeatures(filmDTO.getSpecialFeatures().toArray(new String[0]));
+    }
+    if (filmDTO.getLastUpdate() != null) film.setLastUpdate(Date.from(filmDTO.getLastUpdate().toInstant()));
+    return film;
+  }
+
+  private Language getLanguage(Long id) {
+    Language language = languageService.getLanguageById(id);
+    if (language == null) throw new NotFoundException(
+        "Language for ID " + id + " does not exist"
+    );
+
+    return language;
+  }
+
+  private Category getCategory(Long id) {
+    Category category = categoryService.getCategoryById(id);
+    if (category == null) throw new NotFoundException("Category for ID " + id + " does not exist");
+
+    return category;
   }
 }
