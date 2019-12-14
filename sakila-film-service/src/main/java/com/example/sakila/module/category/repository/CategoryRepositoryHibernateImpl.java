ad10b0e500ee1832c1e819dadc9e8cc5cb78adac
alexDelchev <a.r.delchev@outlook.com>
2019-12-14 19:57:21 +0200

    Add insertCategory hibernate implementation

--- sakila-film-service/src/main/java/com/example/sakila/module/category/repository/CategoryRepositoryHibernateImpl.java
+++ sakila-film-service/src/main/java/com/example/sakila/module/category/repository/CategoryRepositoryHibernateImpl.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
@@ -29,6 +30,15 @@ public class CategoryRepositoryHibernateImpl implements CategoryRepository {
   @Override
   public List<Category> getAllCategories() {
     return createQuery("SELECT c FROM Category c").getResultList();
+  }
+
+  @Override
+  @Transactional
+  public Category insertCategory(Category category) {
+    entityManager.persist(category);
+    entityManager.flush();
+    entityManager.refresh(category);
+    return category;
   }
 
   private TypedQuery<Category> createQuery(String query) {
