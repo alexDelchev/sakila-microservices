689c26081a157ce72438c6ce5040fc5216fa88e5
alexDelchev <a.r.delchev@outlook.com>
2019-12-14 23:17:29 +0200

    Add CategoryServiceTest

---
+++ sakila-film-service/src/test/java/com/example/sakila/module/category/CategoryServiceTest.java
@@ -0,0 +1,20 @@
+package com.example.sakila.module.category;
+
+import com.example.sakila.module.category.repository.CategoryRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class CategoryServiceTest {
+
+  @Mock
+  private CategoryRepository categoryRepository;
+
+  @InjectMocks
+  private CategoryService categoryService;
+  
+}
