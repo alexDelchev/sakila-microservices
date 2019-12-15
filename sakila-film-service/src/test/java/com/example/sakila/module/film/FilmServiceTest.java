980fc76da2032f0c6249a2fa7bb9c9cc63ad8701
alexDelchev <a.r.delchev@outlook.com>
2019-12-15 21:45:46 +0200

    Add FilmServiceTest

---
+++ sakila-film-service/src/test/java/com/example/sakila/module/film/FilmServiceTest.java
@@ -0,0 +1,20 @@
+package com.example.sakila.module.film;
+
+import com.example.sakila.module.film.repository.FilmRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class FilmServiceTest {
+
+  @Mock
+  private FilmRepository filmRepository;
+
+  @InjectMocks
+  private FilmService filmService;
+  
+}
