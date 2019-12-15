9bc24596f6ceea563927cd11043902bd0b5bf69a
alexDelchev <a.r.delchev@outlook.com>
2019-12-16 00:34:12 +0200

    Add updateLanguage test

--- sakila-film-service/src/test/java/com/example/sakila/module/language/LanguageServiceTest.java
+++ sakila-film-service/src/test/java/com/example/sakila/module/language/LanguageServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.sakila.module.language;
 
+import com.example.sakila.exception.NotFoundException;
 import com.example.sakila.module.language.repository.LanguageRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -8,6 +9,8 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class LanguageServiceTest {
@@ -30,5 +33,22 @@ class LanguageServiceTest {
     Language language = languageService.getLanguageByFilmId(null);
 
     assertNull(language);
+  }
+
+  @Test
+  void updateLanguage() {
+    final long EXISTING_LANGUAGE_ID = 1L;
+    when(languageRepository.getLanguageById(EXISTING_LANGUAGE_ID)).thenReturn(new Language());
+
+    final long NON_EXISTING_LANGUAGE_ID = -1L;
+    when(languageRepository.getLanguageById(NON_EXISTING_LANGUAGE_ID)).thenReturn(null);
+
+    when(languageRepository.updateLanguage(any(Language.class))).thenReturn(new Language());
+
+    assertDoesNotThrow(() -> languageService.updateLanguage(EXISTING_LANGUAGE_ID, new Language()));
+    assertThrows(
+        NotFoundException.class,
+        () -> languageService.updateLanguage(NON_EXISTING_LANGUAGE_ID, new Language())
+    );
   }
 }
