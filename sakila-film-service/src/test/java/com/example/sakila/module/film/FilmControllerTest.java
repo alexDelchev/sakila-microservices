67d56f57963dfbdb40a6a6154337921ffbd69a1c
alexDelchev <a.r.delchev@outlook.com>
2019-12-15 22:28:30 +0200

    Add createFilm test

--- sakila-film-service/src/test/java/com/example/sakila/module/film/FilmControllerTest.java
+++ sakila-film-service/src/test/java/com/example/sakila/module/film/FilmControllerTest.java
@@ -1,11 +1,19 @@
 package com.example.sakila.module.film;
 
+import com.example.sakila.exception.NotFoundException;
+import com.example.sakila.generated.server.model.FilmDTO;
+import com.example.sakila.module.category.Category;
+import com.example.sakila.module.category.CategoryService;
+import com.example.sakila.module.language.Language;
+import com.example.sakila.module.language.LanguageService;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class FilmControllerTest {
@@ -13,7 +21,34 @@ class FilmControllerTest {
   @Mock
   private FilmService filmService;
 
+  @Mock
+  private LanguageService languageService;
+
+  @Mock
+  private CategoryService categoryService;
+
   @InjectMocks
   private FilmController filmController;
 
+  @Test
+  void createFilm() {
+    final long EXISTING_LANGUAGE_ID = 1;
+    when(languageService.getLanguageById(EXISTING_LANGUAGE_ID)).thenReturn(new Language());
+
+    final long NON_EXISTING_LANGUAGE_ID = -1L;
+    when(languageService.getLanguageById(NON_EXISTING_LANGUAGE_ID)).thenReturn(null);
+
+    final long NON_EXISTING_CATEGORY_ID = -1L;
+    when(categoryService.getCategoryById(NON_EXISTING_CATEGORY_ID)).thenReturn(null);
+
+    FilmDTO invalidLanguageFilmDTO = new FilmDTO();
+    invalidLanguageFilmDTO.setLanguageId(NON_EXISTING_LANGUAGE_ID);
+
+    FilmDTO invalidCategoryFilmDTO = new FilmDTO();
+    invalidCategoryFilmDTO.setLanguageId(EXISTING_LANGUAGE_ID);
+    invalidCategoryFilmDTO.setCategoryId(NON_EXISTING_CATEGORY_ID);
+
+    assertThrows(NotFoundException.class, () -> filmController.createFilm(invalidLanguageFilmDTO));
+    assertThrows(NotFoundException.class, () -> filmController.createFilm(invalidCategoryFilmDTO));
+  }
 }
