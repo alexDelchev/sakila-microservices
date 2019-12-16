1c7e3a232a9c47ff202e0a66bcbcca60f50866d3
alexDelchev <a.r.delchev@outlook.com>
2019-12-17 00:43:21 +0200

    Add updateCustomer method

--- sakila-payment-service/src/main/java/com/example/sakila/module/customer/CustomerService.java
+++ sakila-payment-service/src/main/java/com/example/sakila/module/customer/CustomerService.java
@@ -1,5 +1,6 @@
 package com.example.sakila.module.customer;
 
+import com.example.sakila.exception.NotFoundException;
 import com.example.sakila.module.customer.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -38,5 +39,23 @@ public class CustomerService {
 
   public Customer createCustomer(Customer customer) {
     return customerRepository.insertCustomer(customer);
+  }
+
+  public Customer updateCustomer(Long id, Customer source) {
+    Customer target = getCustomerById(id);
+    if (target == null) throw new NotFoundException("Customer for ID " + id + " does not exist");
+
+    target.setStore_id(source.getStore_id());
+    target.setFirstName(source.getFirstName());
+    target.setLastName(source.getLastName());
+    target.setEmail(source.getEmail());
+    target.setAddress_id(source.getAddress_id());
+    target.setActive(source.getActive());
+
+    //Update these values only when they are not null as columns prohibit nulls and have default values
+    if (source.getActiveBool() != null) target.setActiveBool(source.getActiveBool());
+    if (source.getCreateDate() != null) target.setCreateDate(source.getCreateDate());
+
+    return customerRepository.updateCustomer(target);
   }
 }
