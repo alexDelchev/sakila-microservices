af225846c26f243b4e9fec4cae73c861a30108a5
alexDelchev <a.r.delchev@outlook.com>
2019-12-18 00:17:37 +0200

    Add udpateRental test

--- sakila-payment-service/src/test/java/com/example/sakila/module/rental/RentalServiceTest.java
+++ sakila-payment-service/src/test/java/com/example/sakila/module/rental/RentalServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.sakila.module.rental;
 
+import com.example.sakila.exception.NotFoundException;
 import com.example.sakila.module.rental.repository.RentalRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,6 +11,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class RentalServiceTest {
@@ -46,5 +49,19 @@ class RentalServiceTest {
     List<Rental> rentals = rentalService.getRentalsByInventoryId(null);
 
     assertNull(rentals);
+  }
+
+  @Test
+  void updateRental() {
+    final long EXISITING_RENTAL_ID = 1L;
+    when(rentalRepository.getRentalById(EXISITING_RENTAL_ID)).thenReturn(new Rental());
+
+    final long NON_EXISTING_RENTAL_ID = -1L;
+    when(rentalRepository.getRentalById(NON_EXISTING_RENTAL_ID)).thenReturn(null);
+
+    when(rentalRepository.updateRental(any(Rental.class))).thenReturn(new Rental());
+
+    assertDoesNotThrow(() -> rentalService.updateRental(EXISITING_RENTAL_ID, new Rental()));
+    assertThrows(NotFoundException.class, () -> rentalService.updateRental(NON_EXISTING_RENTAL_ID, new Rental()));
   }
 }
