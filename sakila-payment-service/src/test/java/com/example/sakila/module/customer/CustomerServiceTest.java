fc46dd52f2f78d68421d26e4e359f7428222cf3a
alexDelchev <a.r.delchev@outlook.com>
2019-12-17 22:47:57 +0200

    Add updateCustomer test

--- sakila-payment-service/src/test/java/com/example/sakila/module/customer/CustomerServiceTest.java
+++ sakila-payment-service/src/test/java/com/example/sakila/module/customer/CustomerServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.sakila.module.customer;
 
+import com.example.sakila.exception.NotFoundException;
 import com.example.sakila.module.customer.repository.CustomerRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,6 +11,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class CustomerServiceTest {
@@ -32,5 +35,22 @@ class CustomerServiceTest {
     List<Customer> customers = customerService.getCustomersByStoreId(null);
 
     assertNull(customers);
+  }
+
+  @Test
+  void updateCustomer() {
+    final long EXISTING_CUSTOMER_ID = 1L;
+    when(customerRepository.getCustomerById(EXISTING_CUSTOMER_ID)).thenReturn(new Customer());
+
+    final long NON_EXISTING_CUSTOMER_ID = -1L;
+    when(customerRepository.getCustomerById(NON_EXISTING_CUSTOMER_ID)).thenReturn(null);
+
+    when(customerRepository.updateCustomer(any(Customer.class))).thenReturn(new Customer());
+
+    assertDoesNotThrow(() -> customerService.updateCustomer(EXISTING_CUSTOMER_ID, new Customer()));
+    assertThrows(
+        NotFoundException.class,
+        () -> customerService.updateCustomer(NON_EXISTING_CUSTOMER_ID, new Customer())
+    );
   }
 }
