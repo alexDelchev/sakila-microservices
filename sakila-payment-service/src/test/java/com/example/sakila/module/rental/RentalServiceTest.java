31704b83d44524737f710edb9782f7ccae0528db
alexDelchev <a.r.delchev@outlook.com>
2019-12-18 00:11:03 +0200

    Add getRentalsByCustomerId test

--- sakila-payment-service/src/test/java/com/example/sakila/module/rental/RentalServiceTest.java
+++ sakila-payment-service/src/test/java/com/example/sakila/module/rental/RentalServiceTest.java
@@ -7,6 +7,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -23,5 +25,12 @@ class RentalServiceTest {
     Rental rental = rentalService.getRentalById(null);
 
     assertNull(rental);
+  }
+
+  @Test
+  void getRentalsByCustomerId() {
+    List<Rental> rentals = rentalService.getRentalsByCustomerId(null);
+
+    assertNull(rentals);
   }
 }
