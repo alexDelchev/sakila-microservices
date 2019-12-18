0a1bf8ff789d84d65ef080bca197145329f2f409
alexDelchev <a.r.delchev@outlook.com>
2019-12-18 08:27:59 +0200

    Add createRental test

--- sakila-payment-service/src/test/java/com/example/sakila/module/rental/RentalControllerTest.java
+++ sakila-payment-service/src/test/java/com/example/sakila/module/rental/RentalControllerTest.java
@@ -1,11 +1,18 @@
 package com.example.sakila.module.rental;
 
+import com.example.sakila.exception.NotFoundException;
+import com.example.sakila.generated.server.model.RentalDTO;
+import com.example.sakila.module.customer.Customer;
 import com.example.sakila.module.customer.CustomerService;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class RentalControllerTest {
@@ -15,5 +22,25 @@ class RentalControllerTest {
 
   @Mock
   private CustomerService customerService;
-  
+
+  @InjectMocks
+  private RentalController rentalController;
+
+  @Test
+  void createRental() {
+    final long EXISTING_CUSTOMER_ID = 1L;
+    when(customerService.getCustomerById(EXISTING_CUSTOMER_ID)).thenReturn(new Customer());
+
+    final long NON_EXISTING_CUSTOMER_ID = -1L;
+
+    when(rentalService.createRental(any(Rental.class))).thenReturn(new Rental());
+
+    RentalDTO validRental = new RentalDTO();
+    validRental.setCustomerId(EXISTING_CUSTOMER_ID);
+    assertDoesNotThrow(() -> rentalController.createRental(validRental));
+
+    RentalDTO invalidRental = new RentalDTO();
+    invalidRental.setCustomerId(NON_EXISTING_CUSTOMER_ID);
+    assertThrows(NotFoundException.class, () -> rentalController.createRental(invalidRental));
+  }
 }
