da825758f0e35319d7ef4d1fd68275c1dab90ebd
alexDelchev <a.r.delchev@outlook.com>
2019-12-18 10:00:46 +0200

    Add replacePayment test

--- sakila-payment-service/src/test/java/com/example/sakila/module/payment/PaymentControllerTest.java
+++ sakila-payment-service/src/test/java/com/example/sakila/module/payment/PaymentControllerTest.java
@@ -67,4 +67,41 @@ class PaymentControllerTest {
     assertDoesNotThrow(() -> paymentController.createPayment(paymentWithValidCustomer));
     assertThrows(NotFoundException.class, () -> paymentController.createPayment(paymentWithInvalidCustomer));
   }
+
+  @Test
+  void replacePayment() {
+    final long NON_EXISTING_RENTAL_ID = -1L;
+    when(rentalService.getRentalById(NON_EXISTING_RENTAL_ID)).thenReturn(null);
+
+    final long EXISTING_RENTAL_ID = 1L;
+    when(rentalService.getRentalById(EXISTING_RENTAL_ID)).thenReturn(new Rental());
+
+    final long NON_EXISTING_CUSTOMER_ID = -1L;
+    when(customerService.getCustomerById(NON_EXISTING_CUSTOMER_ID)).thenReturn(null);
+
+    final long EXISTING_CUSTOMER_ID = 1L;
+    when(customerService.getCustomerById(EXISTING_CUSTOMER_ID)).thenReturn(new Customer());
+
+    when(paymentService.updatePayment(any(Long.class), any(Payment.class))).thenReturn(new Payment());
+
+    PaymentDTO paymentWithValidRental = new PaymentDTO();
+    paymentWithValidRental.setCustomerId(EXISTING_CUSTOMER_ID);
+    paymentWithValidRental.setRentalId(EXISTING_RENTAL_ID);
+
+    PaymentDTO paymentWithInvalidRental = new PaymentDTO();
+    paymentWithInvalidRental.setCustomerId(EXISTING_CUSTOMER_ID);
+    paymentWithInvalidRental.setRentalId(NON_EXISTING_RENTAL_ID);
+
+    PaymentDTO paymentWithValidCustomer = new PaymentDTO();
+    paymentWithValidCustomer.setCustomerId(EXISTING_CUSTOMER_ID);
+
+    PaymentDTO paymentWithInvalidCustomer = new PaymentDTO();
+    paymentWithInvalidCustomer.setCustomerId(NON_EXISTING_CUSTOMER_ID);
+
+    assertDoesNotThrow(() -> paymentController.replacePayment(1L, paymentWithValidRental));
+    assertThrows(NotFoundException.class, () -> paymentController.replacePayment(1L, paymentWithInvalidRental));
+
+    assertDoesNotThrow(() -> paymentController.replacePayment(1L, paymentWithValidCustomer));
+    assertThrows(NotFoundException.class, () -> paymentController.replacePayment(1L, paymentWithInvalidCustomer));
+  }
 }
