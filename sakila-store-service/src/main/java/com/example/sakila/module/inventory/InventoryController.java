f047f16ac8f5129aae593aa600a8a4e13c61dc63
alexDelchev <a.r.delchev@outlook.com>
2019-12-18 22:34:24 +0200

    Implement createInventory api operation

--- sakila-store-service/src/main/java/com/example/sakila/module/inventory/InventoryController.java
+++ sakila-store-service/src/main/java/com/example/sakila/module/inventory/InventoryController.java
@@ -1,14 +1,19 @@
 package com.example.sakila.module.inventory;
 
+import com.example.sakila.exception.NotFoundException;
 import com.example.sakila.generated.server.api.InventoriesApi;
 import com.example.sakila.generated.server.model.InventoryDTO;
+import com.example.sakila.module.store.Store;
+import com.example.sakila.module.store.StoreService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.time.OffsetDateTime;
 import java.time.ZoneId;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -17,9 +22,12 @@ public class InventoryController implements InventoriesApi {
 
   private final InventoryService inventoryService;
 
+  private final StoreService storeService;
+
   @Autowired
-  public InventoryController(InventoryService inventoryService) {
+  public InventoryController(InventoryService inventoryService, StoreService storeService) {
     this.inventoryService = inventoryService;
+    this.storeService = storeService;
   }
 
   @Override
@@ -41,6 +49,11 @@ public class InventoryController implements InventoriesApi {
     );
   }
 
+  @Override
+  public ResponseEntity<InventoryDTO> createInventory(@RequestBody InventoryDTO inventoryDTO) {
+    return ResponseEntity.ok(toDTO(inventoryService.createInventory(toEntity(inventoryDTO))));
+  }
+
   private InventoryDTO toDTO(Inventory inventory) {
     InventoryDTO inventoryDTO = new InventoryDTO();
     inventoryDTO.setId(inventory.getId());
@@ -48,6 +61,25 @@ public class InventoryController implements InventoriesApi {
     inventoryDTO.setStoreId(inventory.getStore().getId());
     inventoryDTO.setLastUpdate(OffsetDateTime.ofInstant(inventory.getLastUpdate().toInstant(), ZoneId.systemDefault()));
     return inventoryDTO;
+  }
+
+  private Inventory toEntity(InventoryDTO inventoryDTO) {
+    Inventory inventory = new Inventory();
+    inventory.setId(inventoryDTO.getId());
+    inventory.setFilm_id(inventoryDTO.getFilmId());
+
+    if (inventoryDTO.getStoreId() != null) {
+      Store store = storeService.getStoreById(inventoryDTO.getStoreId());
+      if (store == null) throw new NotFoundException("Store for ID " + inventoryDTO.getStoreId() + " does not exist");
+
+      inventory.setStore(store);
+    }
+
+    if (inventoryDTO.getLastUpdate() != null) {
+      inventory.setLastUpdate(Date.from(inventoryDTO.getLastUpdate().toInstant()));
+    }
+
+    return inventory;
   }
 
 }
