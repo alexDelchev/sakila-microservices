cd398786466983259ce55e30bb961bd3b6173b7b
alexDelchev <a.r.delchev@outlook.com>
2019-12-19 01:05:24 +0200

    Add updateStaff method

--- sakila-store-service/src/main/java/com/example/sakila/module/staff/StaffService.java
+++ sakila-store-service/src/main/java/com/example/sakila/module/staff/StaffService.java
@@ -1,5 +1,6 @@
 package com.example.sakila.module.staff;
 
+import com.example.sakila.exception.NotFoundException;
 import com.example.sakila.module.staff.repository.StaffRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -28,5 +29,22 @@ public class StaffService {
 
   public Staff createStaff(Staff staff) {
     return staffRepository.insertStaff(staff);
+  }
+
+  public Staff updateStaff(Long id, Staff source) {
+    Staff target = getStaffById(id);
+    if (target == null) throw new NotFoundException("Staff for ID " + id + " does not exist");
+
+    target.setFirstName(source.getFirstName());
+    target.setLastName(source.getLastName());
+    target.setEmail(source.getEmail());
+    target.setStore(source.getStore());
+    target.setUserName(source.getUserName());
+    target.setPassword(source.getPassword());
+    target.setAddress_id(source.getAddress_id());
+
+    if (source.getActive() != null) target.setActive(source.getActive());
+
+    return staffRepository.updateStaff(target);
   }
 }
