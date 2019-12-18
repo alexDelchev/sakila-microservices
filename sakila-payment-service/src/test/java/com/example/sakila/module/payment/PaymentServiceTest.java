2c188eaa5861f33edf8adbf5bc92cb17ee0787a3
alexDelchev <a.r.delchev@outlook.com>
2019-12-18 09:38:59 +0200

    Add PaymentServiceTest

---
+++ sakila-payment-service/src/test/java/com/example/sakila/module/payment/PaymentServiceTest.java
@@ -0,0 +1,20 @@
+package com.example.sakila.module.payment;
+
+import com.example.sakila.module.payment.repository.PaymentRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class PaymentServiceTest {
+
+  @Mock
+  private PaymentRepository paymentRepository;
+
+  @InjectMocks
+  private PaymentService paymentService;
+  
+}
