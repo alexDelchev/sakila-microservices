1c38c8ac41076a37485ba48cc8bf78c6302ed5bc
alexDelchev <a.r.delchev@outlook.com>
2019-12-18 08:39:34 +0200

    Add toEntity conversion method

--- sakila-payment-service/src/main/java/com/example/sakila/module/payment/PaymentController.java
+++ sakila-payment-service/src/main/java/com/example/sakila/module/payment/PaymentController.java
@@ -1,7 +1,12 @@
 package com.example.sakila.module.payment;
 
+import com.example.sakila.exception.NotFoundException;
 import com.example.sakila.generated.server.api.PaymentsApi;
 import com.example.sakila.generated.server.model.PaymentDTO;
+import com.example.sakila.module.customer.Customer;
+import com.example.sakila.module.customer.CustomerService;
+import com.example.sakila.module.rental.Rental;
+import com.example.sakila.module.rental.RentalService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -9,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.time.OffsetDateTime;
 import java.time.ZoneId;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -17,9 +23,17 @@ public class PaymentController implements PaymentsApi {
 
   private final PaymentService paymentService;
 
+  private final CustomerService customerService;
+
+  private final RentalService rentalService;
+
   @Autowired
-  public PaymentController(PaymentService paymentService) {
+  public PaymentController(
+      PaymentService paymentService, CustomerService customerService, RentalService rentalService
+  ) {
     this.paymentService = paymentService;
+    this.customerService = customerService;
+    this.rentalService = rentalService;
   }
 
   @Override
@@ -59,5 +73,32 @@ public class PaymentController implements PaymentsApi {
     paymentDTO.setAmount(payment.getAmount());
     paymentDTO.setPaymentDate(OffsetDateTime.ofInstant(payment.getPaymentDate().toInstant(), ZoneId.systemDefault()));
     return paymentDTO;
+  }
+
+  private Payment toEntity(PaymentDTO paymentDTO) {
+    Payment payment = new Payment();
+    payment.setId(paymentDTO.getId());
+    payment.setAmount(paymentDTO.getAmount());
+    payment.setStaff_id(paymentDTO.getStaffId());
+
+    if (paymentDTO.getCustomerId() != null) {
+      Customer customer = customerService.getCustomerById(paymentDTO.getCustomerId());
+      if (customer == null) throw new NotFoundException(
+          "Customer for ID " + paymentDTO.getCustomerId() + " does not exist"
+      );
+
+      payment.setCustomer(customer);
+    }
+
+    if (paymentDTO.getRentalId() != null) {
+      Rental rental = rentalService.getRentalById(paymentDTO.getRentalId());
+      if (rental == null) throw new NotFoundException("Rental for ID " + paymentDTO.getRentalId() + " does not exist");
+
+      payment.setRental(rental);
+    }
+
+    if (paymentDTO.getPaymentDate() != null) payment.setPaymentDate(Date.from(paymentDTO.getPaymentDate().toInstant()));
+
+    return payment;
   }
 }
