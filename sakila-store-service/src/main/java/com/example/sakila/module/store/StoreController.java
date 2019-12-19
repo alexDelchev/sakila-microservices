4c5721f8c8c976620231506962081148b94bb2d2
alexDelchev <a.r.delchev@outlook.com>
2019-12-20 00:59:18 +0200

    Implement createStore api operation

--- sakila-store-service/src/main/java/com/example/sakila/module/store/StoreController.java
+++ sakila-store-service/src/main/java/com/example/sakila/module/store/StoreController.java
@@ -1,23 +1,31 @@
 package com.example.sakila.module.store;
 
+import com.example.sakila.exception.NotFoundException;
 import com.example.sakila.generated.server.api.StoresApi;
 import com.example.sakila.generated.server.model.StoreDTO;
+import com.example.sakila.module.staff.Staff;
+import com.example.sakila.module.staff.StaffService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.time.OffsetDateTime;
 import java.time.ZoneId;
+import java.util.Date;
 
 @Controller
 public class StoreController implements StoresApi {
 
   private final StoreService storeService;
 
+  private final StaffService staffService;
+
   @Autowired
-  public StoreController(StoreService storeService) {
+  public StoreController(StoreService storeService, StaffService staffService) {
     this.storeService = storeService;
+    this.staffService = staffService;
   }
 
   @Override
@@ -30,6 +38,11 @@ public class StoreController implements StoresApi {
     return ResponseEntity.ok(toDTO(storeService.getStoreByIaddressId(id)));
   }
 
+  @Override
+  public ResponseEntity<StoreDTO> createStore(@RequestBody StoreDTO storeDTO) {
+    return ResponseEntity.ok(toDTO(storeService.createStore(toEntity(storeDTO))));
+  }
+
   private StoreDTO toDTO(Store store) {
     StoreDTO storeDTO = new StoreDTO();
     storeDTO.setId(store.getId());
@@ -37,5 +50,23 @@ public class StoreController implements StoresApi {
     storeDTO.setAddressId(store.getAddress_id());
     storeDTO.setLastUpdate(OffsetDateTime.ofInstant(store.getLastUpdate().toInstant(), ZoneId.systemDefault()));
     return storeDTO;
+  }
+
+  private Store toEntity(StoreDTO storeDTO) {
+    Store store = new Store();
+    store.setId(storeDTO.getId());
+    store.setAddress_id(storeDTO.getAddressId());
+
+    if (storeDTO.getManagerStaffId() != null) {
+      Staff staff = staffService.getStaffById(storeDTO.getManagerStaffId());
+      if (staff == null) throw new NotFoundException(
+          "Staff for ID " + storeDTO.getManagerStaffId() + " does not exist"
+      );
+
+      store.setManagerStaff(staff);
+    }
+
+    if (storeDTO.getLastUpdate() != null) store.setLastUpdate(Date.from(storeDTO.getLastUpdate().toInstant()));
+    return store;
   }
 }
