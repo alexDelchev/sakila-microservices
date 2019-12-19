312367e48b08a27328cabe07b49177849d37f76d
alexDelchev <a.r.delchev@outlook.com>
2019-12-20 00:20:07 +0200

    Add createStaff test

--- sakila-store-service/src/test/java/com/example/sakila/module/staff/StaffControllerTest.groovy
+++ sakila-store-service/src/test/java/com/example/sakila/module/staff/StaffControllerTest.groovy
@@ -1,5 +1,7 @@
 package com.example.sakila.module.staff
 
+import com.example.sakila.exception.NotFoundException
+import com.example.sakila.generated.server.model.StaffDTO
 import com.example.sakila.module.store.StoreService
 import spock.lang.Specification
 
@@ -10,5 +12,17 @@ class StaffControllerTest extends Specification {
   private StaffService staffService = Mock(StaffService)
 
   private StaffController staffController = new StaffController(staffService, storeService)
-  
+
+  void 'createStaff - should throw NotFoundException when DTO references non existing Store'() {
+    given:
+    final long NON_EXISTING_STORE_ID = -1L
+    StaffDTO staffDTO = new StaffDTO()
+    staffDTO.setStoreId(NON_EXISTING_STORE_ID)
+
+    when:
+    staffController.createStaff(staffDTO)
+
+    then:
+    thrown NotFoundException
+  }
 }
