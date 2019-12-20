bc16e4734305c56d7d46ac34aa807afa8eaa94d6
alexDelchev <a.r.delchev@outlook.com>
2019-12-20 02:03:17 +0200

    Add createStore test

--- sakila-store-service/src/test/java/com/example/sakila/module/store/StoreControllerTest.groovy
+++ sakila-store-service/src/test/java/com/example/sakila/module/store/StoreControllerTest.groovy
@@ -1,5 +1,7 @@
 package com.example.sakila.module.store
 
+import com.example.sakila.exception.NotFoundException
+import com.example.sakila.generated.server.model.StoreDTO
 import com.example.sakila.module.staff.StaffService
 import spock.lang.Specification
 
@@ -8,5 +10,19 @@ class StoreControllerTest extends Specification {
   private StaffService staffService = Mock(StaffService)
 
   private StoreService storeService = Mock(StoreService)
-  
+
+  private StoreController storeController = new StoreController(storeService, staffService)
+
+  void 'createStore - should throw NotFoundException'() {
+    given:
+    final long NON_EXISTING_STAFF_ID = -1
+    StoreDTO storeDTO = new StoreDTO()
+    storeDTO.setManagerStaffId(NON_EXISTING_STAFF_ID)
+
+    when:
+    storeController.createStore(storeDTO)
+
+    then:
+    thrown NotFoundException
+  }
 }
