9039f64d9ad436c2569733b8b1fac6c7c83912aa
alexDelchev <a.r.delchev@outlook.com>
2019-12-20 02:00:00 +0200

    Add StoreControllerTest

---
+++ sakila-store-service/src/test/java/com/example/sakila/module/store/StoreControllerTest.groovy
@@ -0,0 +1,12 @@
+package com.example.sakila.module.store
+
+import com.example.sakila.module.staff.StaffService
+import spock.lang.Specification
+
+class StoreControllerTest extends Specification {
+
+  private StaffService staffService = Mock(StaffService)
+
+  private StoreService storeService = Mock(StoreService)
+  
+}
