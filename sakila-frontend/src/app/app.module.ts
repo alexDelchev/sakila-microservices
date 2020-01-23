83e4f7a73553e08d707e1299fa9c8862cb7d1ea5
alexDelchev <a.r.delchev@outlook.com>
2020-01-23 18:58:33 +0200

    Add ApiRequestInterceptor to AppModule providers

--- sakila-frontend/src/app/app.module.ts
+++ sakila-frontend/src/app/app.module.ts
@@ -1,8 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Provider, forwardRef } from '@angular/core';
+
+import { environment } from '../environments/environment';
+
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { ApiRequestInterceptor } from './config/request.interceptor';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+export const API_INTERCEPTOR_PROVIDER: Provider = {
+  provide: HTTP_INTERCEPTORS,
+  useExisting: forwardRef(() => ApiRequestInterceptor),
+  multi: true
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +23,9 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    ApiRequestInterceptor, API_INTERCEPTOR_PROVIDER
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
