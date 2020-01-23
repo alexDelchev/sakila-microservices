7fd2c8e7798ef4f3765c0cd0f6aedb93d221b0c4
alexDelchev <a.r.delchev@outlook.com>
2020-01-23 22:38:37 +0200

    Add StoreService

---
+++ sakila-frontend/src/app/services/store/store.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+import { Observable } from 'rxjs';
+
+import { ApiService } from '@api/generated/store/services/api.service';
+import { StoreDTO } from '@api/generated/store/models/store-dto';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StoreService {
+
+  constructor(private apiService: ApiService) { }
+
+}
