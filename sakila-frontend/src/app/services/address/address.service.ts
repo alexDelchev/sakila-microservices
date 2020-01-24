a1dd68ee617e3479813d174b86c68f777546ce46
alexDelchev <a.r.delchev@outlook.com>
2020-01-24 10:08:58 +0200

    Add wrappers for read api operations

--- sakila-frontend/src/app/services/address/address.service.ts
+++ sakila-frontend/src/app/services/address/address.service.ts
@@ -12,4 +12,15 @@ export class AddressService {
 
   constructor(private apiService: ApiService) { }
 
+  getAddressById(id: number): Observable<AddressDTO> {
+    return this.apiService.getAddressById(id);
+  }
+
+  getAddressesByCityId(id: number): Observable<Array<AddressDTO>> {
+    return this.apiService.getAddressesByCityId(id);
+  }
+
+  getAddressesByCountryId(id: number): Observable<Array<AddressDTO>> {
+    return this.apiService.getAddressesByCountryId(id);
+  }
 }
