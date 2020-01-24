885be21c449b1b2208c125aec3a3a4e26d5d3783
alexDelchev <a.r.delchev@outlook.com>
2020-01-24 20:57:42 +0200

    Add wrappers for read api operations

--- sakila-frontend/src/app/services/rental/rental.service.ts
+++ sakila-frontend/src/app/services/rental/rental.service.ts
@@ -12,4 +12,19 @@ export class RentalService {
 
   constructor(private apiService: ApiService) { }
 
+  getRentalById(id: number): Observable<RentalDTO> {
+    return this.apiService.getRentalById(id);
+  }
+
+  getRentalsByInventoryId(id: number): Observable<Array<RentalDTO>> {
+    return this.apiService.getRentalsByInventoryId(id);
+  }
+
+  getRentalsByCustomerId(id: number): Observable<Array<RentalDTO>> {
+    return this.apiService.getRentalsByCustomerId(id);
+  }
+
+  getRentalsByStaffId(id: number): Observable<Array<RentalDTO>> {
+    return this.apiService.getRentalsByStaffId(id);
+  }
 }
