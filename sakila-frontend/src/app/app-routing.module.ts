0d08f4f6d7529a7d241acadde671f5eae6e01437
alexDelchev <a.r.delchev@outlook.com>
2020-01-26 23:17:19 +0200

    Add paths to routing module

--- sakila-frontend/src/app/app-routing.module.ts
+++ sakila-frontend/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { CategoryPickerComponent } from './category-picker/category-picker.component';
+import { FilmBrowserComponent } from './film-browser/film-browser.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CategoryPickerComponent },
+  { path: 'category/:categoryId', component: FilmBrowserComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
