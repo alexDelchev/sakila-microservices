5c62c1a8f5d1c8ca47627c21dc0628ba09a4a5d7
alexDelchev <a.r.delchev@outlook.com>
2020-01-27 00:32:07 +0200

    Add select field to change category selection

--- sakila-frontend/src/app/film-browser/film-browser.component.ts
+++ sakila-frontend/src/app/film-browser/film-browser.component.ts
@@ -4,6 +4,8 @@ import { Location } from '@angular/common';
 
 import { FilmDTO } from '@api/generated/film/models/film-dto';
 import { FilmService } from '../services/film/film.service';
+import { CategoryDTO } from '@api/generated/film/models/category-dto';
+import { CategoryService } from '../services/category/category.service';
 
 @Component({
   selector: 'app-film-browser',
@@ -12,19 +14,30 @@ import { FilmService } from '../services/film/film.service';
 })
 export class FilmBrowserComponent implements OnInit {
 
+  private category: CategoryDTO;
+
+  private categories: Array<CategoryDTO>;
+
   private films: Array<FilmDTO>;
 
   constructor(
     private route: ActivatedRoute,
     private location: Location,
-    private filmService: FilmService
+    private filmService: FilmService,
+    private categoryService: CategoryService
   ) { }
 
   ngOnInit() {
+    this.categoryService.getAllCategories().subscribe(result => this.categories = result);
+
     let categoryId: number = this.getNumberParameterFromRoute('categoryId');
     if (categoryId) {
       this.filmService.getFilmsByCategoryId(categoryId).subscribe(result => this.films = result);
     }
+  }
+
+  changeCategorySelection() {
+    this.filmService.getFilmsByCategoryId(this.category.id).subscribe(result => this.films = result);
   }
 
   private getNumberParameterFromRoute(name: string): number {
