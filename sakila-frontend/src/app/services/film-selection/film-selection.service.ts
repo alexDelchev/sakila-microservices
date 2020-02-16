9cd0db8551520547a0eea1affb76d71e9267e469
alexDelchev <a.r.delchev@outlook.com>
2020-02-16 19:19:41 +0200

    Add read, write operations for an array if numbers stored in session memory

--- sakila-frontend/src/app/services/film-selection/film-selection.service.ts
+++ sakila-frontend/src/app/services/film-selection/film-selection.service.ts
@@ -5,5 +5,29 @@ import { Injectable } from '@angular/core';
 })
 export class FilmSelectionService {
 
+  STORAGE_KEY: string = 'sakila-film-selection-film-ids';
+
   constructor() { }
+
+  addFilmToSelection(filmId: number) {
+    let selectedItems: Array<number> = this.getSelectedItems();
+    if (selectedItems === null) selectedItems = new Array();
+
+    if (selectedItems.indexOf(filmId) == -1) {
+      selectedItems.push(filmId);
+      this.setSelectedItems(selectedItems);
+    }
+  }
+
+  getSelectedFilmIds(): Array<number> {
+    return this.getSelectedItems();
+  }
+
+  private getSelectedItems(): Array<number> {
+    return JSON.parse(sessionStorage.getItem(this.STORAGE_KEY));
+  }
+
+  private setSelectedItems(selectedItems: Array<number>) {
+    sessionStorage.setItem(this.STORAGE_KEY, JSON.stringify(selectedItems));
+  }
 }
