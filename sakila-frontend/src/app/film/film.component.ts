f6f458f41ae3f98351df139d1bac63eebeefb674
alexDelchev <a.r.delchev@outlook.com>
2020-02-19 08:32:09 +0200

    Add onclick function which selects/deselects film

--- sakila-frontend/src/app/film/film.component.ts
+++ sakila-frontend/src/app/film/film.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 
 import { FilmService } from '../services/film/film.service';
 import { FilmDTO } from '../api/generated/film/models/film-dto';
+import { FilmSelectionService } from '../services/film-selection/film-selection.service';
 
 @Component({
   selector: 'app-film',
@@ -11,5 +12,30 @@ import { FilmDTO } from '../api/generated/film/models/film-dto';
 export class FilmComponent {
 
   @Input() film: FilmDTO;
+
+  constructor(private filmSelectionService: FilmSelectionService) {}
+
+  isFilmSelected(): boolean {
+    let selection: Array<number> = this.filmSelectionService.getSelectedFilmIds();
+    if (selection == null) return false
+
+    return selection.indexOf(this.film.id) > -1
+  }
+
+  doClick() {
+    if (this.isFilmSelected()) {
+      this.deselectFilm();
+    } else {
+      this.selectFilm();
+    }
+  }
+
+  selectFilm() {
+    this.filmSelectionService.addFilmToSelection(this.film.id);
+  }
+
+  deselectFilm() {
+    this.filmSelectionService.removeFilmFromSelection(this.film.id);
+  }
 
 }
