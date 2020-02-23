4dc3a1ddddadc09e7d6ec8e26558c0de1e48b2e4
alexDelchev <a.r.delchev@outlook.com>
2020-02-23 18:40:10 +0200

    Define routing path for CartComponent

--- sakila-frontend/src/app/app-routing.module.ts
+++ sakila-frontend/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { CategoryPickerComponent } from './category-picker/category-picker.component';
 import { FilmBrowserComponent } from './film-browser/film-browser.component';
+import { CartComponent } from './cart/cart.component';
 
 const routes: Routes = [
   { path: '', component: CategoryPickerComponent },
-  { path: 'category/:categoryId', component: FilmBrowserComponent }
+  { path: 'category/:categoryId', component: FilmBrowserComponent },
+  { path: 'cart', component: CartComponent }
 ];
 
 @NgModule({
