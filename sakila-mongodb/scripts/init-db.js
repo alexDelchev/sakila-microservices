62165fdb969a9a335424a5a8bccb415399c8b98d
alexDelchev <a.r.delchev@outlook.com>
2020-03-23 00:27:29 +0200

    Fix credentials

--- sakila-mongodb/scripts/init-db.js
+++ sakila-mongodb/scripts/init-db.js
@@ -3,8 +3,8 @@ db.auth('sakila-dba', 'sakila-dba-pass')
 db = db.getSiblingDB('sakila-film')
 
 db.createUser({
-  user: 'sakila-film-service',
-  pwd: 'sakila-dba-pass',
+  user: 'sakila_film_service',
+  pwd: 'sakila_dba_pass',
   roles: [
     {
       role: 'readWrite',
