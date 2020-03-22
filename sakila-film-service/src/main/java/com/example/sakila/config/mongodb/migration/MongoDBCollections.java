eb2181731f24a520298687ebeee31aad9a33c35f
alexDelchev <a.r.delchev@outlook.com>
2020-03-23 00:22:25 +0200

    Add MongoDBCollections model

---
+++ sakila-film-service/src/main/java/com/example/sakila/config/mongodb/migration/MongoDBCollections.java
@@ -0,0 +1,36 @@
+package com.example.sakila.config.mongodb.migration;
+
+import java.util.List;
+
+public class MongoDBCollections {
+
+  private List<String> create;
+
+  private List<MongoDBCollectionUpdate> update;
+
+  private List<MongoDBCollectionData> data;
+
+  public List<String> getCreate() {
+    return create;
+  }
+
+  public void setCreate(List<String> create) {
+    this.create = create;
+  }
+
+  public List<MongoDBCollectionUpdate> getUpdate() {
+    return update;
+  }
+
+  public void setUpdate(List<MongoDBCollectionUpdate> update) {
+    this.update = update;
+  }
+
+  public List<MongoDBCollectionData> getData() {
+    return data;
+  }
+
+  public void setData(List<MongoDBCollectionData> data) {
+    this.data = data;
+  }
+}
