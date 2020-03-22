d0a939b4f022e2f8801ed45f1ff3973bf9e18b2c
alexDelchev <a.r.delchev@outlook.com>
2020-03-23 00:26:56 +0200

    Add MongoDBConfig

---
+++ sakila-film-service/src/main/java/com/example/sakila/config/MongoDBConfig.java
@@ -0,0 +1,54 @@
+package com.example.sakila.config;
+
+
+import com.example.sakila.config.mongodb.migration.MongoDBMigrationRunner;
+import com.mongodb.*;
+import com.mongodb.client.MongoDatabase;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Collections;
+
+import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
+import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
+
+@Configuration
+public class MongoDBConfig {
+
+  @Value("${mongodb.host}")
+  private String host;
+
+  @Value("${mongodb.port}")
+  private Integer port;
+
+  @Value("${mongodb.user}")
+  private String user;
+
+  @Value("${mongodb.password}")
+  private String password;
+
+  @Value("${mongodb.database}")
+  private String databaseName;
+
+  @Bean
+  public MongoClient mongoClient() {
+    ServerAddress address = new ServerAddress(host, port);
+    MongoCredential credential = MongoCredential.createCredential(user, databaseName, password.toCharArray());
+    return new MongoClient(address, credential, MongoClientOptions.builder().build());
+  }
+
+  @Bean
+  public MongoDatabase mongoDatabase(MongoClient mongoClient) {
+    CodecRegistry pojoCodecRegistry = fromProviders(PojoCodecProvider.builder().automatic(true).build());
+    CodecRegistry codecRegistry = fromRegistries(MongoClientSettings.getDefaultCodecRegistry(), pojoCodecRegistry);
+    MongoDatabase database = mongoClient.getDatabase(databaseName).withCodecRegistry(codecRegistry);
+
+    MongoDBMigrationRunner migrationRunner = new MongoDBMigrationRunner(database);
+    migrationRunner.runMigrations();
+
+    return database;
+  }
+}
