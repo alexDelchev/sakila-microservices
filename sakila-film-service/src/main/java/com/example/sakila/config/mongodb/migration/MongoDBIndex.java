6e211dffab510b5be81206f7f5c7fa5e3161b086
alexDelchev <a.r.delchev@outlook.com>
2020-03-23 00:20:18 +0200

    Add MongoDBIndex model

---
+++ sakila-film-service/src/main/java/com/example/sakila/config/mongodb/migration/MongoDBIndex.java
@@ -0,0 +1,32 @@
+package com.example.sakila.config.mongodb.migration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MongoDBIndex {
+
+  public MongoDBIndex(String type) {
+    this.type = type;
+    this.fields = new ArrayList<>();
+  }
+
+  private String type;
+
+  private List<String> fields;
+
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public List<String> getFields() {
+    return fields;
+  }
+
+  public void setFields(List<String> fields) {
+    this.fields = fields;
+  }
+}
