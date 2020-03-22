a9f08cdad12aed20e31229bf02188a6c1489707e
alexDelchev <a.r.delchev@outlook.com>
2020-03-23 00:22:03 +0200

    Add MongoDBCollectionUpdate model

---
+++ sakila-film-service/src/main/java/com/example/sakila/config/mongodb/migration/MongoDBCollectionUpdate.java
@@ -0,0 +1,32 @@
+package com.example.sakila.config.mongodb.migration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MongoDBCollectionUpdate {
+
+  public MongoDBCollectionUpdate(String collectionName) {
+    this.collectionName = collectionName;
+    this.indicesToBeCreated = new ArrayList<>();
+  }
+
+  private String collectionName;
+
+  private List<MongoDBIndex> indicesToBeCreated;
+
+  public String getCollectionName() {
+    return collectionName;
+  }
+
+  public void setCollectionName(String collectionName) {
+    this.collectionName = collectionName;
+  }
+
+  public List<MongoDBIndex> getIndicesToBeCreated() {
+    return indicesToBeCreated;
+  }
+
+  public void setIndicesToBeCreated(List<MongoDBIndex> indicesToBeCreated) {
+    this.indicesToBeCreated = indicesToBeCreated;
+  }
+}
