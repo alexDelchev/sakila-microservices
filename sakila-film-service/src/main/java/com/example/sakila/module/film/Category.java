ce39226a46b40e248eec9c331832814e0ee8d48b
alexDelchev <a.r.delchev@outlook.com>
2020-03-23 21:16:02 +0200

    Add Category enum

---
+++ sakila-film-service/src/main/java/com/example/sakila/module/film/Category.java
@@ -0,0 +1,22 @@
+package com.example.sakila.module.film;
+
+public enum Category {
+
+      Action,
+      Animation,
+      Children,
+      Classics,
+      Comedy,
+      Documentary,
+      Drama,
+      Family,
+      Foreign,
+      Games,
+      Horror,
+      Music,
+      New,
+      SciFi,
+      Sports,
+      Travel
+  
+}
