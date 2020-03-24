d8c39f83b731ce0504b896126c916c8beb6d31a4
alexDelchev <a.r.delchev@outlook.com>
2020-03-24 22:12:10 +0200

    Add ActorUtils

---
+++ sakila-film-service/src/main/java/com/example/sakila/module/actor/ActorUtils.java
@@ -0,0 +1,30 @@
+package com.example.sakila.module.actor;
+
+import com.example.sakila.generated.server.model.ActorDTO;
+import org.bson.types.ObjectId;
+
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+public class ActorUtils {
+
+  public static ActorDTO toDTO(Actor actor) {
+    ActorDTO actorDTO = new ActorDTO();
+    actorDTO.setId(actor.getId().toHexString());
+    actorDTO.setFirstName(actor.getFirstName());
+    actorDTO.setLastName(actor.getLastName());
+    actorDTO.setLastUpdate(OffsetDateTime.ofInstant(actor.getLastUpdate().toInstant(), ZoneId.systemDefault()));
+    return actorDTO;
+  }
+
+  public static Actor toEntity(ActorDTO actorDTO) {
+    Actor actor = new Actor();
+    actor.setId(new ObjectId(actorDTO.getId()));
+    actor.setFirstName(actorDTO.getFirstName());
+    actor.setLastName(actorDTO.getLastName());
+    if (actorDTO.getLastUpdate() != null) actor.setLastUpdate(Date.from(actorDTO.getLastUpdate().toInstant()));
+    return actor;
+  }
+
+}
