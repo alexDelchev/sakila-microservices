50cdd3c2ca78425df2a1819da41bf6252f8d81bc
alexDelchev <a.r.delchev@outlook.com>
2020-03-26 00:57:47 +0200

    Add Inventory model

---
+++ sakila-film-service/src/main/java/com/example/sakila/module/film/Inventory.java
@@ -0,0 +1,24 @@
+package com.example.sakila.module.film;
+
+public class Inventory {
+
+  private Long storeId;
+
+  private Integer quantity;
+
+  public Long getStoreId() {
+    return storeId;
+  }
+
+  public void setStoreId(Long storeId) {
+    this.storeId = storeId;
+  }
+
+  public Integer getQuantity() {
+    return quantity;
+  }
+
+  public void setQuantity(Integer quantity) {
+    this.quantity = quantity;
+  }
+}
