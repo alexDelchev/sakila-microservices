991b656ab0841243f618c44e888e9844d8a80423
alexDelchev <a.r.delchev@outlook.com>
2020-03-29 01:04:00 +0200

    Change name of category paremeter to 'category'

--- sakila-frontend/src/app/app-routing.module.ts
+++ sakila-frontend/src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { CartComponent } from './cart/cart.component';
 
 const routes: Routes = [
   { path: '', component: CategoryPickerComponent },
-  { path: 'category/:categoryId', component: FilmBrowserComponent },
+  { path: 'category/:category', component: FilmBrowserComponent },
   { path: 'cart', component: CartComponent }
 ];
 
