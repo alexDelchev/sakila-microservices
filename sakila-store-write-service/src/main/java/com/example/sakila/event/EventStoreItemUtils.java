e40430ec2aa45d0ec33fc66f405d4b006c12f77a
alexDelchev <a.r.delchev@outlook.com>
2020-04-15 08:34:53 +0300

    Add event serialization/deserialization (JSON) methods

--- sakila-store-write-service/src/main/java/com/example/sakila/event/EventStoreItemUtils.java
+++ sakila-store-write-service/src/main/java/com/example/sakila/event/EventStoreItemUtils.java
@@ -7,5 +7,20 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class EventStoreItemUtils {
 
   private final static ObjectMapper mapper = new ObjectMapper();
-  
+
+  private static <T> T deserialize(String json, Class<T> type) {
+    try {
+      return mapper.readValue(json, type);
+    } catch (JsonProcessingException e) {
+      throw new IllegalArgumentException(String.format("Failed parsing json (%s)", json), e);
+    }
+  }
+
+  private static String serialize(Object object) {
+    try {
+      return mapper.writeValueAsString(object);
+    } catch (JsonProcessingException e) {
+      throw new IllegalArgumentException(String.format("Failed to serialize: %s", e.getMessage()), e);
+    }
+  }
 }
