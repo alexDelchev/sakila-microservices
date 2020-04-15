87af5cdb8b769f9d3bc167c48cfb965bbe8cf5ba
alexDelchev <a.r.delchev@outlook.com>
2020-04-16 01:15:02 +0300

    Add DeleteStaffCommand

---
+++ sakila-store-write-service/src/main/java/com/example/sakila/module/staff/command/model/DeleteStaffCommand.java
@@ -0,0 +1,14 @@
+package com.example.sakila.module.staff.command.model;
+
+public class DeleteStaffCommand {
+
+  private Long staffId;
+
+  public Long getStaffId() {
+    return staffId;
+  }
+
+  public void setStaffId(Long staffId) {
+    this.staffId = staffId;
+  }
+}
