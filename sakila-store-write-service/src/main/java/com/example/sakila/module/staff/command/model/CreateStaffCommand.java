c9ffa5d5913194fc53c2a8b1ea1e7728e9ef2431
alexDelchev <a.r.delchev@outlook.com>
2020-04-16 01:14:45 +0300

    Add CreateStaffCommand

---
+++ sakila-store-write-service/src/main/java/com/example/sakila/module/staff/command/model/CreateStaffCommand.java
@@ -0,0 +1,84 @@
+package com.example.sakila.module.staff.command.model;
+
+public class CreateStaffCommand {
+
+  private String firstName;
+
+  private String lastName;
+
+  private Long addressId;
+
+  private String email;
+
+  private Long storeId;
+
+  private Boolean active;
+
+  private String userName;
+
+  private String password;
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public Long getAddressId() {
+    return addressId;
+  }
+
+  public void setAddressId(Long addressId) {
+    this.addressId = addressId;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public Long getStoreId() {
+    return storeId;
+  }
+
+  public void setStoreId(Long storeId) {
+    this.storeId = storeId;
+  }
+
+  public Boolean getActive() {
+    return active;
+  }
+
+  public void setActive(Boolean active) {
+    this.active = active;
+  }
+
+  public String getUserName() {
+    return userName;
+  }
+
+  public void setUserName(String userName) {
+    this.userName = userName;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+}
