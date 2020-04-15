d95eb5239c95e27ad9cd43ffab951c1afa194854
alexDelchev <a.r.delchev@outlook.com>
2020-04-15 08:14:57 +0300

    Add event Constants
    Default encoding is defined as UTF-8

---
+++ sakila-store-write-service/src/main/java/com/example/sakila/event/Constants.java
@@ -0,0 +1,9 @@
+package com.example.sakila.event;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+public class Constants {
+
+  static final Charset CHARSET = StandardCharsets.UTF_8;
+}
