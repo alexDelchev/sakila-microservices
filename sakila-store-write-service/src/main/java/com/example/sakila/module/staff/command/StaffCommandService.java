4ce91df2f845d15e7aa94327e412c5dba2dcef6f
alexDelchev <a.r.delchev@outlook.com>
2020-04-16 01:20:36 +0300

    Add handlers for the basic staff commands

--- sakila-store-write-service/src/main/java/com/example/sakila/module/staff/command/StaffCommandService.java
+++ sakila-store-write-service/src/main/java/com/example/sakila/module/staff/command/StaffCommandService.java
@@ -28,6 +28,46 @@ public class StaffCommandService {
     this.eventBus.register(this);
   }
 
+  @Handler
+  public void onChangeActiveCommand(ChangeActiveCommand command) {
+    processBasicCommand(command, new ActiveChangedEvent());
+  }
+
+  @Handler
+  public void onChangeAddressCommand(ChangeAddressCommand command) {
+    processBasicCommand(command, new AddressChangedEvent());
+  }
+
+  @Handler
+  public void onChangeEmailCommand(ChangeEmailCommand command) {
+    processBasicCommand(command, new EmailChangedEvent());
+  }
+
+  @Handler
+  public void onChangeFirstNameCommand(ChangeFirstNameCommand command) {
+    processBasicCommand(command, new FirstNameChangedEvent());
+  }
+
+  @Handler
+  public void onChangeLastNameCommand(ChangeLastNameCommand command) {
+    processBasicCommand(command, new LastNameChangedEvent());
+  }
+
+  @Handler
+  public void onChangePasswordCommand(ChangePasswordCommand command) {
+    processBasicCommand(command, new PasswordChangedEvent());
+  }
+
+  @Handler
+  public void onChangeStoreCommand(ChangeStoreCommand command) {
+    processBasicCommand(command, new StoreChangedEvent());
+  }
+
+  @Handler
+  public void onChangeUsernameCommand(ChangeUsernameCommand command) {
+    processBasicCommand(command, new UsernameChangedEvent());
+  }
+
   private <T> void processBasicCommand(BasicStaffCommand<T> command, BasicStaffEvent<T> event) {
     event.setStaffId(command.getStaffId());
     event.setNewValue(command.getNewValue());
