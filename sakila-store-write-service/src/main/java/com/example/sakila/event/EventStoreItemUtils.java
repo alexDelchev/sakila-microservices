9aa2aa0648af8519fa253aedec00a63d85782335
alexDelchev <a.r.delchev@outlook.com>
2020-04-15 08:33:33 +0300

    Add EventStoreItemUtils calss
    This calss will be used to define event conversions

---
+++ sakila-store-write-service/src/main/java/com/example/sakila/event/EventStoreItemUtils.java
@@ -0,0 +1,11 @@
+package com.example.sakila.event;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class EventStoreItemUtils {
+
+  private final static ObjectMapper mapper = new ObjectMapper();
+  
+}
