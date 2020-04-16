520520a3658d654d768b31593307cf9b93637d87
alexDelchev <a.r.delchev@outlook.com>
2020-04-16 08:15:33 +0300

    Add StaffWriteModel

---
+++ sakila-store-write-service/src/main/java/com/example/sakila/module/staff/StaffWriteModel.java
@@ -0,0 +1,109 @@
+package com.example.sakila.module.staff;
+
+import java.util.Date;
+
+public class StaffWriteModel {
+
+  private Long id;
+
+  private Long version;
+
+  private String firstName;
+
+  private String lastName;
+
+  private Long address_id;
+
+  private String email;
+
+  private Long storeId;
+
+  private Boolean active;
+
+  private String userName;
+
+  private String password;
+
+  public StaffWriteModel() {
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Long getVersion() {
+    return version;
+  }
+
+  public void setVersion(Long version) {
+    this.version = version;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public Long getAddress_id() {
+    return address_id;
+  }
+
+  public void setAddress_id(Long address_id) {
+    this.address_id = address_id;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public Long getStoreId() {
+    return storeId;
+  }
+
+  public void setStoreId(Long storeId) {
+    this.storeId = storeId;
+  }
+
+  public Boolean getActive() {
+    return active;
+  }
+
+  public void setActive(Boolean active) {
+    this.active = active;
+  }
+
+  public String getUserName() {
+    return userName;
+  }
+
+  public void setUserName(String userName) {
+    this.userName = userName;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+}
