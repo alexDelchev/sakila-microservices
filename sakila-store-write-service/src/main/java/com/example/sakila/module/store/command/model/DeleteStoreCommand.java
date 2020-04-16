819acbf5d8f0a92ef9f72d23075ce4839ddb7f32
alexDelchev <a.r.delchev@outlook.com>
2020-04-16 08:20:47 +0300

    Add DeleteStoreCommand

---
+++ sakila-store-write-service/src/main/java/com/example/sakila/module/store/command/model/DeleteStoreCommand.java
@@ -0,0 +1,14 @@
+package com.example.sakila.module.store.command.model;
+
+public class DeleteStoreCommand {
+
+  private Long storeId;
+
+  public Long getStoreId() {
+    return storeId;
+  }
+
+  public void setStoreId(Long storeId) {
+    this.storeId = storeId;
+  }
+}
