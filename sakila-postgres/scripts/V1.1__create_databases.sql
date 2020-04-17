a572eddeef8d2c59a02007ed1832306a314903e5
alexDelchev <a.r.delchev@outlook.com>
2020-04-17 17:09:27 +0300

    Rename sakila_store to sakila_store_read

--- sakila-postgres/scripts/V1.1__create_databases.sql
+++ sakila-postgres/scripts/V1.1__create_databases.sql
@@ -37,18 +37,18 @@ CREATE DATABASE sakila_payment
 
 
 
-CREATE DATABASE sakila_store
+CREATE DATABASE sakila_store_read
     WITH
     OWNER = postgres
     ENCODING = 'UTF8'
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1;
 
-GRANT ALL ON DATABASE sakila_store TO sakila_store_service;
+GRANT ALL ON DATABASE sakila_store_read TO sakila_store_read_service;
 
-GRANT ALL ON DATABASE sakila_store TO postgres;
+GRANT ALL ON DATABASE sakila_store_read TO postgres;
 
-GRANT TEMPORARY, CONNECT ON DATABASE sakila_store TO PUBLIC;
+GRANT TEMPORARY, CONNECT ON DATABASE sakila_store_read TO PUBLIC;
 
 CREATE DATABASE sakila_store_write
     WITH
