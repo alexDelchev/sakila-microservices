51bd812e8c323136be4aca4d7096a4e455f23ea1
alexDelchev <a.r.delchev@outlook.com>
2020-04-18 20:19:03 +0300

    Add id (UUID), version (Long) properties
    These will be used for event checking

--- sakila-store-read-service/src/main/java/com/example/sakila/data/staff/StaffDeletedEvent.java
+++ sakila-store-read-service/src/main/java/com/example/sakila/data/staff/StaffDeletedEvent.java
@@ -1,8 +1,30 @@
 package com.example.sakila.data.staff;
 
+import java.util.UUID;
+
 public class StaffDeletedEvent {
 
+  private UUID id;
+
+  private Long version;
+
   private Long staffId;
+
+  public UUID getId() {
+    return id;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public Long getVersion() {
+    return version;
+  }
+
+  public void setVersion(Long version) {
+    this.version = version;
+  }
 
   public Long getStaffId() {
     return staffId;
