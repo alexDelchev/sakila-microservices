981dba5b67758413963fdcbe0c517e0126317c60
alexDelchev <a.r.delchev@outlook.com>
2020-04-18 20:10:20 +0300

    Add processed_event table
    This table will contain a log of the processed events coming from the event streams

---
+++ sakila-store-read-service/src/main/resources/db/migration/V0.05__create_processed_event_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE processed_event (
+    event_id uuid NOT NULL,
+    processed_date timestamp without time zone DEFAULT now() NOT NULL
+);
+
+ALTER TABLE public.processed_event OWNER TO "${user}";
+
+ALTER TABLE ONLY processed_event
+    ADD CONSTRAINT processed_event_pkey PRIMARY KEY (event_id);
