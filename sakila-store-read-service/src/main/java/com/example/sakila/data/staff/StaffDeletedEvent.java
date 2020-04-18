22204eac8c5acf5d1a62d219629d314210e26682
alexDelchev <a.r.delchev@outlook.com>
2020-04-18 08:23:24 +0300

    Add StaffDeletedEvent

---
+++ sakila-store-read-service/src/main/java/com/example/sakila/data/staff/StaffDeletedEvent.java
@@ -0,0 +1,14 @@
+package com.example.sakila.data.staff;
+
+public class StaffDeletedEvent {
+
+  private Long staffId;
+
+  public Long getStaffId() {
+    return staffId;
+  }
+
+  public void setStaffId(Long staffId) {
+    this.staffId = staffId;
+  }
+}
