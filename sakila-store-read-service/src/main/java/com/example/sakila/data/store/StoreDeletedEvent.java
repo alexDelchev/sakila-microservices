351412910a2eda053fc0475dedaa76796460a4dc
alexDelchev <a.r.delchev@outlook.com>
2020-04-18 20:45:47 +0300

    Add id (UUID), version (Long) properties

--- sakila-store-read-service/src/main/java/com/example/sakila/data/store/StoreDeletedEvent.java
+++ sakila-store-read-service/src/main/java/com/example/sakila/data/store/StoreDeletedEvent.java
@@ -1,8 +1,30 @@
 package com.example.sakila.data.store;
 
+import java.util.UUID;
+
 public class StoreDeletedEvent {
 
+  private UUID id;
+
+  private Long version;
+
   private Long storeId;
+
+  public UUID getId() {
+    return id;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public Long getVersion() {
+    return version;
+  }
+
+  public void setVersion(Long version) {
+    this.version = version;
+  }
 
   public Long getStoreId() {
     return storeId;
