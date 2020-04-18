5486c1a6d4009c2664757cff929b6d9b2d56871f
alexDelchev <a.r.delchev@outlook.com>
2020-04-18 08:06:52 +0300

    Fix date conversion

--- sakila-store-read-service/src/main/java/com/example/sakila/module/staff/StaffController.java
+++ sakila-store-read-service/src/main/java/com/example/sakila/module/staff/StaffController.java
@@ -1,9 +1,7 @@
 package com.example.sakila.module.staff;
 
-import com.example.sakila.exception.NotFoundException;
 import com.example.sakila.generated.server.api.StaffApi;
 import com.example.sakila.generated.server.model.StaffDTO;
-import com.example.sakila.module.store.Store;
 import com.example.sakila.module.store.StoreService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.sql.Date;
+import java.time.Instant;
 import java.time.OffsetDateTime;
 import java.time.ZoneId;
 import java.util.List;
@@ -70,7 +69,10 @@ public class StaffController implements StaffApi {
     staffDTO.setActive(staff.getActive());
     staffDTO.setUserName(staff.getUserName());
     staffDTO.setPassword(staff.getPassword());
-    staffDTO.setLastUpdate(OffsetDateTime.ofInstant(staff.getLastUpdate().toInstant(), ZoneId.systemDefault()));
+    if (staff.getLastUpdate() != null) {
+      staffDTO.setLastUpdate(
+          OffsetDateTime.ofInstant(Instant.ofEpochMilli(staff.getLastUpdate().getTime()), ZoneId.systemDefault()));
+    }
 
     return staffDTO;
   }
