1ccb7ac44c247e78c33c427d8523de99441a452c
alexDelchev <a.r.delchev@outlook.com>
2020-04-18 08:17:14 +0300

    Add StaffDTO
    This dto will be received by the kafka listeners

---
+++ sakila-store-read-service/src/main/java/com/example/sakila/data/staff/StaffDTO.java
@@ -0,0 +1,106 @@
+package com.example.sakila.data.staff;
+
+import java.util.Date;
+
+public class StaffDTO {
+
+  private Long id;
+
+  private String firstName;
+
+  private String lastName;
+
+  private Long addressId;
+
+  private String email;
+
+  private Long storeId;
+
+  private Boolean active;
+
+  private String userName;
+
+  private String password;
+
+  private Date lastUpdate;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public Long getAddressId() {
+    return addressId;
+  }
+
+  public void setAddressId(Long addressId) {
+    this.addressId = addressId;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public Long getStoreId() {
+    return storeId;
+  }
+
+  public void setStoreId(Long storeId) {
+    this.storeId = storeId;
+  }
+
+  public Boolean getActive() {
+    return active;
+  }
+
+  public void setActive(Boolean active) {
+    this.active = active;
+  }
+
+  public String getUserName() {
+    return userName;
+  }
+
+  public void setUserName(String userName) {
+    this.userName = userName;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public Date getLastUpdate() {
+    return lastUpdate;
+  }
+
+  public void setLastUpdate(Date lastUpdate) {
+    this.lastUpdate = lastUpdate;
+  }
+}
