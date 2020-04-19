217a2ce335effe6cad2dbfb3f3038326bfc32dab
alexDelchev <a.r.delchev@outlook.com>
2020-04-19 23:12:32 +0300

    Add StaffCommandService test spec

---
+++ sakila-store-write-service/src/test/java/com/example/sakila/module/staff/command/StaffCommandServiceTest.groovy
@@ -0,0 +1,18 @@
+package com.example.sakila.module.staff.command
+
+import com.example.sakila.event.EventService
+import com.example.sakila.event.bus.EventBus
+import com.example.sakila.exception.NotFoundException
+import com.example.sakila.module.staff.StaffWriteModel
+import com.example.sakila.module.staff.command.model.DeleteStaffCommand
+import spock.lang.Specification
+
+class StaffCommandServiceTest extends Specification {
+
+  private EventBus eventBus = Mock(EventBus)
+
+  private EventService eventService = Mock(EventService)
+
+  private StaffCommandService commandService = new StaffCommandService(eventBus, eventService)
+  
+}
