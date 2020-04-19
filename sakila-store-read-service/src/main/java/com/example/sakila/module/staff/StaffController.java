f3284145e0668e74dcb9dc1761ec8dbbe19c683d
alexDelchev <a.r.delchev@outlook.com>
2020-04-20 00:40:32 +0300

    Remove unused import

--- sakila-store-read-service/src/main/java/com/example/sakila/module/staff/StaffController.java
+++ sakila-store-read-service/src/main/java/com/example/sakila/module/staff/StaffController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.sql.Date;
 import java.time.Instant;
