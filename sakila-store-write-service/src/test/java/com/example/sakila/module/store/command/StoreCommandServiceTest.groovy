f38885ec9369b17675a508385e1da7ef27ce0ad2
alexDelchev <a.r.delchev@outlook.com>
2020-04-19 11:06:45 +0300

    Add ChangeManagerCommand handler tests

--- sakila-store-write-service/src/test/java/com/example/sakila/module/store/command/StoreCommandServiceTest.groovy
+++ sakila-store-write-service/src/test/java/com/example/sakila/module/store/command/StoreCommandServiceTest.groovy
@@ -3,8 +3,10 @@ package com.example.sakila.module.store.command
 import com.example.sakila.event.EventService
 import com.example.sakila.event.bus.EventBus
 import com.example.sakila.exception.NotFoundException
+import com.example.sakila.module.staff.StaffWriteModel
 import com.example.sakila.module.store.command.model.ChangeAddressCommand
 import com.example.sakila.module.store.StoreWriteModel
+import com.example.sakila.module.store.command.model.ChangeManagerCommand
 import com.example.sakila.module.store.command.model.DeleteStoreCommand
 import spock.lang.Specification
 
@@ -42,6 +44,40 @@ class StoreCommandServiceTest extends Specification {
 
     when:
     storeCommandService.onChangeAddressCommand(changeAddressCommand)
+
+    then:
+    thrown NotFoundException
+  }
+
+  def "OnChangeManagerCommand - should throw NotFoundException on storeId"() {
+    given:
+    Long nonExistingStoreId = -1L
+    eventService.aggregateExists(nonExistingStoreId, StoreWriteModel.class) >> false
+    ChangeManagerCommand changeManagerCommand = new ChangeManagerCommand(
+        storeId: nonExistingStoreId,
+        newValue: 1L
+    )
+
+    when:
+    storeCommandService.onChangeManagerCommand(changeManagerCommand)
+
+    then:
+    thrown NotFoundException
+  }
+
+  def "OnChangeManagerCommand - should throw NotFoundException on staffId"() {
+    given:
+    Long existingStoreId = 1L
+    Long nonExistingStaffId = -1L;
+    eventService.aggregateExists(existingStoreId, StoreWriteModel.class) >> true
+    eventService.aggregateExists(nonExistingStaffId, StaffWriteModel.class) >> false
+    ChangeManagerCommand changeManagerCommand = new ChangeManagerCommand(
+        storeId: existingStoreId,
+        newValue: nonExistingStaffId
+    )
+
+    when:
+    storeCommandService.onChangeManagerCommand(changeManagerCommand)
 
     then:
     thrown NotFoundException
