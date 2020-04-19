69697638f78d5f51f0446b0b673c7ea6255a460b
alexDelchev <a.r.delchev@outlook.com>
2020-04-20 00:29:12 +0300

    Add getVersion test

--- sakila-store-write-service/src/test/java/com/example/sakila/event/EventServiceTest.groovy
+++ sakila-store-write-service/src/test/java/com/example/sakila/event/EventServiceTest.groovy
@@ -8,5 +8,16 @@ class EventServiceTest extends Specification {
   private EventStore eventStore = Mock(EventStore)
 
   private EventService eventService = new EventService(eventStore)
-  
+
+  def "GetAggregateVersion"() {
+    given:
+    Long nonExistingAggregateId = -1L
+    eventStore.getAggregateVersion(nonExistingAggregateId) >> null
+
+    when:
+    Long version = eventService.getAggregateVersion(nonExistingAggregateId)
+
+    then:
+    version == 0L
+  }
 }
