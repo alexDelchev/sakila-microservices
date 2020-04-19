8d7671cb949488760516b82b8e7037d6ec24f4fe
alexDelchev <a.r.delchev@outlook.com>
2020-04-19 11:02:15 +0300

    Add ChangeAddressCommand handler test

--- sakila-store-write-service/src/test/java/com/example/sakila/module/store/command/StoreCommandServiceTest.groovy
+++ sakila-store-write-service/src/test/java/com/example/sakila/module/store/command/StoreCommandServiceTest.groovy
@@ -3,6 +3,7 @@ package com.example.sakila.module.store.command
 import com.example.sakila.event.EventService
 import com.example.sakila.event.bus.EventBus
 import com.example.sakila.exception.NotFoundException
+import com.example.sakila.module.store.command.model.ChangeAddressCommand
 import com.example.sakila.module.store.StoreWriteModel
 import com.example.sakila.module.store.command.model.DeleteStoreCommand
 import spock.lang.Specification
@@ -25,6 +26,22 @@ class StoreCommandServiceTest extends Specification {
 
     when:
     storeCommandService.onDeleteStoreCommand(deleteStoreCommand)
+
+    then:
+    thrown NotFoundException
+  }
+
+  def "OnChangeAddressCommand"() {
+    given:
+    Long nonExistingStoreId = -1L
+    eventService.aggregateExists(nonExistingStoreId, StoreWriteModel.class) >> false
+    ChangeAddressCommand changeAddressCommand = new ChangeAddressCommand(
+        storeId: nonExistingStoreId,
+        newValue: 1L
+    )
+
+    when:
+    storeCommandService.onChangeAddressCommand(changeAddressCommand)
 
     then:
     thrown NotFoundException
