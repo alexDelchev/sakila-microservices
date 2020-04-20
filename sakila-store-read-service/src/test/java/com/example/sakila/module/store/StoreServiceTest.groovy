be5c035c4993f87be281af1780f6d577d03ec36f
alexDelchev <a.r.delchev@outlook.com>
2020-04-20 10:04:32 +0300

    Remove write operation tests

--- sakila-store-read-service/src/test/java/com/example/sakila/module/store/StoreServiceTest.groovy
+++ sakila-store-read-service/src/test/java/com/example/sakila/module/store/StoreServiceTest.groovy
@@ -1,8 +1,5 @@
 package com.example.sakila.module.store
 
-import com.example.sakila.exception.DataConflictException
-import com.example.sakila.exception.NotFoundException
-import com.example.sakila.module.staff.Staff
 import com.example.sakila.module.store.repository.StoreRepository
 import spock.lang.Specification
 
@@ -43,83 +40,5 @@ class StoreServiceTest extends Specification {
 
     then:
     store == null
-  }
-
-  void 'createStore - should throw DataConflictException when managerStaffId is not unique'() {
-    given:
-    final long EXISTING_STAFF_ID = 1L
-    Staff managerStaff = new Staff()
-    managerStaff.setId(EXISTING_STAFF_ID)
-
-    Store store = new Store()
-    store.setManagerStaff(managerStaff)
-
-    final long EXISTING_OTHER_STAFF_ID = 2L
-    Staff otherStaff = new Staff()
-    otherStaff.setId(EXISTING_OTHER_STAFF_ID)
-
-    Store otherStore = new Store()
-    otherStore.setManagerStaff(otherStaff)
-
-    storeRepository.getStoreByManagerStaffId(_ as Long) >> new Store()
-    storeRepository.getStoreById(_ as Long) >> store
-
-    when:
-    storeService.createStore(otherStore)
-
-    then:
-    thrown DataConflictException
-  }
-
-  void 'updateStore - should throw DataConflictException'() {
-    given:
-    final long EXISTING_STAFF_ID = 1L
-    Staff managerStaff = new Staff()
-    managerStaff.setId(EXISTING_STAFF_ID)
-
-    final long STORE_ID = 1L
-    Store store = new Store()
-    store.setId(STORE_ID)
-    store.setManagerStaff(managerStaff)
-
-    final long EXISTING_OTHER_STAFF_ID = 2L
-    Staff otherStaff = new Staff()
-    otherStaff.setId(EXISTING_OTHER_STAFF_ID)
-
-    final long OTHER_STORE_ID = 2L
-    Store otherStore = new Store()
-    otherStore.setId(OTHER_STORE_ID)
-    otherStore.setManagerStaff(otherStaff)
-
-    storeRepository.getStoreByManagerStaffId(EXISTING_OTHER_STAFF_ID) >> otherStore
-    storeRepository.getStoreById(STORE_ID) >> store
-
-    when:
-    storeService.updateStore(STORE_ID, otherStore)
-
-    then:
-    thrown DataConflictException
-  }
-
-  void 'updateStore - should throw NotFoundException'() {
-    given:
-    final long NON_EXISTING_STORE_ID = -1L;
-
-    when:
-    storeService.updateStore(NON_EXISTING_STORE_ID, new Store())
-
-    then:
-    thrown NotFoundException
-  }
-
-  void 'deleteStore - should throw NotFoundException'() {
-    given:
-    final long NON_EXISTING_STORE_ID = -1L;
-
-    when:
-    storeService.deleteStore(NON_EXISTING_STORE_ID)
-
-    then:
-    thrown NotFoundException
   }
 }
