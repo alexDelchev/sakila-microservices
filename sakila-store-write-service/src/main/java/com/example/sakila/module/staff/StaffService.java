561eb0e4b81db9db483f41a94245c46aa8f1c509
alexDelchev <a.r.delchev@outlook.com>
2020-04-22 21:36:17 +0300

    Add getStaffAtEvent method

--- sakila-store-write-service/src/main/java/com/example/sakila/module/staff/StaffService.java
+++ sakila-store-write-service/src/main/java/com/example/sakila/module/staff/StaffService.java
@@ -5,6 +5,8 @@ import com.example.sakila.module.staff.StaffWriteModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 public class StaffService {
 
@@ -20,6 +22,15 @@ public class StaffService {
     StaffWriteModel staff = new StaffWriteModel();
     staff.setId(id);
     eventService.getEventsForAggregate(id, StaffWriteModel.class).forEach(e -> e.apply(staff));
+
+    return staff;
+  }
+
+  public StaffWriteModel getStaffAtEvent(Long id, UUID eventId) {
+    if (id == null) return null;
+    StaffWriteModel staff = new StaffWriteModel();
+    staff.setId(id);
+    eventService.getEventsForAggregateUpToEvent(id, eventId, StaffWriteModel.class).forEach(e -> e.apply(staff));
 
     return staff;
   }
