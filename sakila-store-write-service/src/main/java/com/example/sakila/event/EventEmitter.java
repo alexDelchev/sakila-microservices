f0e4e28e8e3fd7bb91561aff1da6798c7fb0018e
alexDelchev <a.r.delchev@outlook.com>
2020-04-22 21:58:46 +0300

    Add EventEmitter component
    This component will be used to listen to a topic and re-emit stored events

---
+++ sakila-store-write-service/src/main/java/com/example/sakila/event/EventEmitter.java
@@ -0,0 +1,67 @@
+package com.example.sakila.event;
+
+import com.example.sakila.event.bus.EventBus;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+public class EventEmitter {
+
+  private static final String LISTENER_GROUP_ID = "sakila-store-write-event-emitter";
+
+  private static final String TRIGGER_EVENTS_TOPIC = "store-store-write-events-trigger";
+
+  private final ObjectMapper mapper = new ObjectMapper();
+
+  private final EventService eventService;
+
+  private final EventBus storeEventBus;
+
+  private final EventBus staffEventBus;
+
+  @Autowired
+  public EventEmitter(
+      EventService eventService,
+      @Qualifier("StoreEventBus") EventBus storeEventBus,
+      @Qualifier("StaffEventBus") EventBus staffEventBus
+  ) {
+    this.eventService = eventService;
+    this.storeEventBus = storeEventBus;
+    this.staffEventBus = staffEventBus;
+  }
+
+  private void emitAllEvents() {
+    eventService.getAllEvents().forEach(this::emitEvent);
+  }
+
+  private void emitSubsequentEvents(UUID eventId) {
+    eventService.getSubsequentEvents(eventId).forEach(this::emitEvent);
+  }
+
+  private void emitEvent(Event event) {
+    System.out.println("emitting " + event.getVersion());
+    if (storeEventBus.hasHandlerForType(event.getClass())) {
+      storeEventBus.emitSynchroniously(event);
+    } else if (staffEventBus.hasHandlerForType(event.getClass())) {
+      staffEventBus.emitSynchroniously(event);
+    } else {
+      throw new IllegalArgumentException(
+          String.format("Could not find event bus for type %s", event.getClass().getTypeName())
+      );
+    }
+  }
+
+  private <T> T deserialize(String message, Class<T> type) {
+    try {
+      return mapper.readValue(message, type);
+    } catch (JsonProcessingException e) {
+      throw new IllegalArgumentException(String.format("Failed to parse json %s", message), e);
+    }
+  }
+}
