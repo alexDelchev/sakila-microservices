a1a796f542868f9d0004ba0302bd5f9ec6fd646f
alexDelchev <a.r.delchev@outlook.com>
2020-04-29 02:06:16 +0300

    Update store service according to the new api

--- sakila-frontend/src/app/services/store/store.service.ts
+++ sakila-frontend/src/app/services/store/store.service.ts
@@ -2,33 +2,42 @@ import { Injectable } from '@angular/core';
 
 import { Observable } from 'rxjs';
 
-import { ApiService } from '@api/generated/store/services/api.service';
-import { StoreDTO } from '@api/generated/store/models/store-dto';
+import { ApiService as ReadApiService } from '@api/generated/store/read/services/api.service';
+import { ApiService as WriteApiService } from '@api/generated/store/write/services/api.service';
+import { StoreDTO } from '@api/generated/store/read/models/store-dto';
+import { CreateStoreCommandDTO } from '@api/generated/store/write/models/create-store-command-dto';
+import { DeleteStoreCommandDTO } from '@api/generated/store/write/models/delete-store-command-dto';
+import { BasicInt64CommandDTO } from '@api/generated/store/write/models/basic-int-64command-dto';
+import { AggregateIdDTO } from '@api/generated/store/write/models/aggregate-id-dto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StoreService {
 
-  constructor(private apiService: ApiService) { }
+  constructor(private readApiService: ReadApiService, private writeApiService: WriteApiService) { }
 
   getStoreById(id: number): Observable<StoreDTO> {
-    return this.apiService.getStoreById(id);
+    return this.readApiService.getStoreById(id);
   }
 
   getStoreByAddressId(id: number): Observable<StoreDTO> {
-    return this.apiService.getStoreByAddressId(id);
+    return this.readApiService.getStoreByAddressId(id);
   }
 
-  createStore(store: StoreDTO): Observable<StoreDTO> {
-    return this.apiService.createStore(store);
+  createStore(command: CreateStoreCommandDTO): Observable<AggregateIdDTO> {
+    return this.writeApiService.createStore(command);
   }
 
-  replaceStore(id: number, store: StoreDTO): Observable<StoreDTO> {
-    return this.apiService.replaceStore({ id: id, storeDTO: store });
+  deleteStore(command: DeleteStoreCommandDTO) {
+    this.writeApiService.deleteStore(command);
   }
 
-  deleteStore(id: number) {
-    this.apiService.deleteStore(id);
+  changeAddress(command: BasicInt64CommandDTO) {
+    this.writeApiService.changeAddress(command);
+  }
+
+  changeManager(command: BasicInt64CommandDTO) {
+    this.writeApiService.changeManager(command);
   }
 }
