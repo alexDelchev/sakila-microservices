553526666c51dc00fac71dc17c14b007762be5c2
alexDelchev <a.r.delchev@outlook.com>
2020-04-29 18:19:43 +0300

    Split public.rental.inventory column into film_id, store_id
    Inventory data is now stored as an embedded object of the film mongodb collection, so the inventory column does not make sense here

--- sakila-payment-service/src/main/resources/db/migration/V0.04__create_rental_table.sql
+++ sakila-payment-service/src/main/resources/db/migration/V0.04__create_rental_table.sql
@@ -9,7 +9,8 @@ ALTER TABLE public.rental_rental_id_seq OWNER TO "${user}";
 CREATE TABLE rental (
     rental_id integer DEFAULT nextval('rental_rental_id_seq'::regclass) NOT NULL,
     rental_date timestamp without time zone NOT NULL,
-    inventory_id integer NOT NULL,
+    film_id character varying(100) NOT NULL,
+    store_id integer NOT NULL,
     customer_id smallint NOT NULL,
     return_date timestamp without time zone,
     staff_id smallint NOT NULL,
@@ -21,9 +22,9 @@ ALTER TABLE public.rental OWNER TO "${user}";
 ALTER TABLE ONLY rental
     ADD CONSTRAINT rental_pkey PRIMARY KEY (rental_id);
 
-CREATE INDEX idx_fk_inventory_id ON rental USING btree (inventory_id);
+CREATE INDEX idx_film_id_store_id ON rental USING btree (film_id, store_id);
 
-CREATE UNIQUE INDEX idx_unq_rental_rental_date_inventory_id_customer_id ON rental USING btree (rental_date, inventory_id, customer_id);
+CREATE UNIQUE INDEX idx_unq_rental_rental_date_film_id_store_id_customer_id ON rental USING btree (rental_date, film_id, store_id, customer_id);
 
 CREATE TRIGGER last_updated
     BEFORE UPDATE ON rental
