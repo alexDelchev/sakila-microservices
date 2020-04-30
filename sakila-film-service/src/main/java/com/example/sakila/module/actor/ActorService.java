6436f9046b9512141ea3d665821fd50ffed4c95a
alexDelchev <a.r.delchev@outlook.com>
2020-04-30 19:37:42 +0300

    Remove unused imports

--- sakila-film-service/src/main/java/com/example/sakila/module/actor/ActorService.java
+++ sakila-film-service/src/main/java/com/example/sakila/module/actor/ActorService.java
@@ -10,9 +10,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Service
 public class ActorService {
 
