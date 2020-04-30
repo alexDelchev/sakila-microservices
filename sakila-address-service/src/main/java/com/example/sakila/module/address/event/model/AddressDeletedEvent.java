09fc0146ffd3e30b47dfa0b7cf203e1516ffa0b8
alexDelchev <a.r.delchev@outlook.com>
2020-04-30 23:54:07 +0300

    Add AddressDeletedEvent

---
+++ sakila-address-service/src/main/java/com/example/sakila/module/address/event/model/AddressDeletedEvent.java
@@ -0,0 +1,14 @@
+package com.example.sakila.module.address.event.model;
+
+public class AddressDeletedEvent {
+
+  private Long id;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+}
