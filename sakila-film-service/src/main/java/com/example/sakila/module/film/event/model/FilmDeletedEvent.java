2724f4e4a34c27d4eda440951bbb3cd14a24a8a0
alexDelchev <a.r.delchev@outlook.com>
2020-04-30 20:11:08 +0300

    Add FilmDeletedEvent

---
+++ sakila-film-service/src/main/java/com/example/sakila/module/film/event/model/FilmDeletedEvent.java
@@ -0,0 +1,14 @@
+package com.example.sakila.module.film.event.model;
+
+public class FilmDeletedEvent {
+
+  private String id;
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+}
