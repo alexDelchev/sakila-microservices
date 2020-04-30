72e4522a8d9c5a580507361d6c88549249328442
alexDelchev <a.r.delchev@outlook.com>
2020-04-30 08:52:52 +0300

    Add CustomerEventEmitter
    Initial

---
+++ sakila-payment-service/src/main/java/com/example/sakila/module/customer/event/CustomerEventEmitter.java
@@ -0,0 +1,45 @@
+package com.example.sakila.module.customer.event;
+
+import com.example.sakila.event.bus.EventBus;
+import com.example.sakila.event.bus.Handler;
+import com.example.sakila.module.customer.event.model.CustomerCreatedEvent;
+import com.example.sakila.module.customer.event.model.CustomerDeletedEvent;
+import com.example.sakila.module.customer.event.model.CustomerUpdatedEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomerEventEmitter {
+
+  private static final String CUSTOMER_CREATED_TOPIC = "sakila-payment-customer-created";
+
+  private static final String CUSTOMER_UPDATED_TOPIC = "sakila-payment-customer-updated";
+
+  private static final String CUSTOMER_DELETED_TOPIC = "sakila-payment-customer-deleted";
+
+  private final ObjectMapper objectMapper = new ObjectMapper();
+
+  private final EventBus eventBus;
+
+  private final KafkaTemplate<String, String> kafkaTemplate;
+
+  @Autowired
+  public CustomerEventEmitter(@Qualifier("CustomerEventBus") EventBus eventBus, KafkaTemplate kafkaTemplate) {
+    this.eventBus = eventBus;
+    this.kafkaTemplate = kafkaTemplate;
+
+    this.eventBus.register(this);
+  }
+
+  private String serialize(Object object) {
+    try {
+      return objectMapper.writeValueAsString(object);
+    } catch (JsonProcessingException e) {
+      throw new IllegalArgumentException(String.format("Failed parsing json: %s", e.getMessage()), e);
+    }
+  }
+}
