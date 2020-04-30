745ad58fd51fd8febd0a7c9e3bc9ae89dc39e904
alexDelchev <a.r.delchev@outlook.com>
2020-04-30 08:58:27 +0300

    Add PaymentEventEmitter
    Initial

---
+++ sakila-payment-service/src/main/java/com/example/sakila/module/payment/event/PaymentEventEmitter.java
@@ -0,0 +1,45 @@
+package com.example.sakila.module.payment.event;
+
+import com.example.sakila.event.bus.EventBus;
+import com.example.sakila.event.bus.Handler;
+import com.example.sakila.module.payment.event.model.PaymentCreatedEvent;
+import com.example.sakila.module.payment.event.model.PaymentDeletedEvent;
+import com.example.sakila.module.payment.event.model.PaymentUpdatedEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PaymentEventEmitter {
+
+  private static final String PAYMENT_CREATE_TOPIC = "sakila-payment-payment-create";
+
+  private static final String PAYMENT_UPDATE_TOPIC = "sakila-payment-payment-update";
+
+  private static final String PAYMENT_DELETE_TOPIC = "sakila-payment-payment-delete";
+
+  private final ObjectMapper objectMapper = new ObjectMapper();
+
+  private final EventBus eventBus;
+
+  private final KafkaTemplate<String, String> kafkaTemplate;
+
+  @Autowired
+  public PaymentEventEmitter(@Qualifier("PaymentEventBus") EventBus eventBus, KafkaTemplate kafkaTemplate) {
+    this.eventBus = eventBus;
+    this.kafkaTemplate = kafkaTemplate;
+
+    this.eventBus.register(this);
+  }
+
+  private String serialize(Object object) {
+    try {
+      return objectMapper.writeValueAsString(object);
+    } catch (JsonProcessingException e) {
+      throw new IllegalArgumentException(String.format("Failed parsing json: %s", e.getMessage()), e);
+    }
+  }
+}
