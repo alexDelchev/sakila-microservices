690c56ef98639aff1c23728e30fddaa6e4dffc22
alexDelchev <a.r.delchev@outlook.com>
2020-04-30 20:47:21 +0300

    Add CountryEventEmitter
    Initial

---
+++ sakila-address-service/src/main/java/com/example/sakila/module/country/event/CountryEventEmitter.java
@@ -0,0 +1,41 @@
+package com.example.sakila.module.country.event;
+
+import com.example.sakila.event.bus.EventBus;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CountryEventEmitter {
+
+  private static final String COUNTRY_CREATED_TOPIC = "sakila-address-country-created";
+
+  private static final String COUNTRY_UPDATED_TOPIC = "sakila-address-country-updated";
+
+  private static final String COUNTRY_DELETED_TOPIC = "sakila-address-country-deleted";
+
+  private final ObjectMapper objectMapper = new ObjectMapper();
+
+  private final EventBus eventBus;
+
+  private final KafkaTemplate<String, String> kafkaTemplate;
+
+  @Autowired
+  public CountryEventEmitter(@Qualifier("CountryEventBus") EventBus eventBus, KafkaTemplate kafkaTemplate) {
+    this.eventBus = eventBus;
+    this.kafkaTemplate = kafkaTemplate;
+
+    this.eventBus.register(this);
+  }
+
+  private String serialize(Object object) {
+    try {
+      return objectMapper.writeValueAsString(object);
+    } catch (JsonProcessingException e) {
+      throw new IllegalArgumentException(e);
+    }
+  }
+}
