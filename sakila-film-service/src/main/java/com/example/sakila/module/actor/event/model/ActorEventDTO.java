e6e04e6ed43c774c93c1dacf3aac22baa5f71ddc
alexDelchev <a.r.delchev@outlook.com>
2020-04-30 19:21:49 +0300

    Add ActorEventDTO
    This will be used as the main part of the event message payload

---
+++ sakila-film-service/src/main/java/com/example/sakila/module/actor/event/model/ActorEventDTO.java
@@ -0,0 +1,46 @@
+package com.example.sakila.module.actor.event.model;
+
+import java.util.Date;
+
+public class ActorEventDTO {
+
+  private String id;
+
+  private String firstName;
+
+  private String lastName;
+
+  private Date lastUpdate;
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public Date getLastUpdate() {
+    return lastUpdate;
+  }
+
+  public void setLastUpdate(Date lastUpdate) {
+    this.lastUpdate = lastUpdate;
+  }
+}
