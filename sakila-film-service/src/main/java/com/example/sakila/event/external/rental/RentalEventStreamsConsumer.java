fd45d8672aa83c29500f7741fb37dc5ff72cda03
alexDelchev <a.r.delchev@outlook.com>
2020-05-01 08:29:32 +0300

    Add RentalEventStreamsConsumer
    This class listens for created rentals and decreases the available film quantity for the given store

---
+++ sakila-film-service/src/main/java/com/example/sakila/event/external/rental/RentalEventStreamsConsumer.java
@@ -0,0 +1,42 @@
+package com.example.sakila.event.external.rental;
+
+import com.example.sakila.event.external.rental.model.RentalCreatedEvent;
+import com.example.sakila.event.external.rental.model.RentalEventDTO;
+import com.example.sakila.module.film.FilmService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RentalEventStreamsConsumer {
+
+  private static final String GROUP_ID = "sakila-film-rental-consumer";
+
+  private static final String RENTAL_CREATED_TOPIC = "sakila-payment-rental-created";
+
+  private final ObjectMapper objectMapper = new ObjectMapper();
+
+  private final FilmService filmService;
+
+  @Autowired
+  public RentalEventStreamsConsumer(FilmService filmService) {
+    this.filmService = filmService;
+  }
+
+  @KafkaListener(topics = {RENTAL_CREATED_TOPIC}, groupId = GROUP_ID)
+  public void onRentalCreated(String message) {
+    RentalCreatedEvent event = deserialize(message, RentalCreatedEvent.class);
+    RentalEventDTO dto = event.getDto();
+    filmService.decreaseQuantityForStore(dto.getFilmId(),dto.getStoreId());
+  }
+
+  private <T> T deserialize(String json, Class<T> type) {
+    try {
+      return objectMapper.readValue(json, type);
+    } catch(JsonProcessingException e) {
+      throw new IllegalArgumentException(e);
+    }
+  }
+}
