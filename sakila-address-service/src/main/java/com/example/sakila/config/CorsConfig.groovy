d2fe4769e3a05367141ecf7ad7a0476ff9672fce
alexDelchev <a.r.delchev@outlook.com>
2020-05-02 07:17:03 +0300

    Rewrite CorsConfig in groovy

---
+++ sakila-address-service/src/main/java/com/example/sakila/config/CorsConfig.groovy
@@ -0,0 +1,20 @@
+package com.example.sakila.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class CorsConfig {
+
+  @Bean
+  WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping('/**').allowedMethods('*')
+      }
+    }
+  }
+}
