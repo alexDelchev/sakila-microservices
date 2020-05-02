64e29dc5780daf3832c514970eddcf0a21c3dd71
alexDelchev <a.r.delchev@outlook.com>
2020-05-02 07:35:21 +0300

    Convert EventBus to groovy

---
+++ sakila-address-service/src/main/java/com/example/sakila/event/bus/EventBus.groovy
@@ -0,0 +1,62 @@
+package com.example.sakila.event.bus
+
+import java.lang.reflect.Method
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.ConcurrentHashMap
+
+class EventBus {
+
+  private final Map<Class<?>, List<HandlerMethod>> handlers
+
+  final String name
+
+  EventBus(String name) {
+    this.name = name
+    handlers = new ConcurrentHashMap<>()
+  }
+
+  boolean hasHandlerForType(Class type) {
+    type in handlers.keySet()
+  }
+
+  void emit(Object object) {
+    Class<?> type = object.class
+
+    if (handlers.containsKey(type)) {
+      handlers[type].each { h -> CompletableFuture.runAsync { h.invoke(object) } }
+    }
+  }
+
+  void emitSynchronously(Object object) {
+    Class<?> type = object.class
+
+    if (handlers.containsKey(type)) {
+      handlers[type].each { h -> h.invoke(object) }
+    }
+  }
+
+  void register(Object object) {
+    List<HandlerMethod> eventHandlers = getEventHandlerMethods(object)
+
+    eventHandlers.each { h ->
+      if (handlers.containsKey(h.parameterType)) {
+        handlers[h.parameterType] << h
+      } else {
+        handlers[h.parameterType] = [h]
+      }
+    }
+  }
+
+  private List<HandlerMethod> getEventHandlerMethods(Object object) {
+    object.class.declaredMethods.findAll { it.isAnnotationPresent(Handler) }.collect { m ->
+      checkEventHandlerMethodParameters(m)
+      new HandlerMethod(object, m, m.parameterTypes[0])
+    }
+  }
+
+  private void checkEventHandlerMethodParameters(Method method) {
+    if (method.parameterCount > 1) {
+      throw new IllegalArgumentException("Method annotated with @Handler has more than 1 parameter")
+    }
+  }
+}
