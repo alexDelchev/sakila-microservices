880aac8c6a4c7a621154dd01d715e4f7b1b1decf
alexDelchev <a.r.delchev@outlook.com>
2020-05-07 23:42:36 +0300

    Convert EventBus to kotlin

---
+++ sakila-payment-service/src/main/java/com/example/sakila/event/bus/EventBus.kt
@@ -0,0 +1,61 @@
+package com.example.sakila.event.bus
+
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.lang.IllegalArgumentException
+import java.lang.reflect.Method
+import java.util.concurrent.ConcurrentHashMap
+
+class EventBus(private val name: String) {
+
+  private val handlers: MutableMap<Class<*>, MutableList<HandlerMethod>>
+
+  init {
+    this.handlers = ConcurrentHashMap()
+  }
+
+  fun hasHandlerForType(type: Class<*>): Boolean = handlers.containsKey(type)
+
+  fun emit(payload: Any) {
+    val type = payload::class.java
+
+    if (handlers.containsKey(type)) {
+      handlers[type]!!.forEach { GlobalScope.launch { it.invoke(payload)} }
+    }
+  }
+
+  fun emitSynchronously(payload: Any) {
+    val type = payload::class.java
+
+    if (handlers.containsKey(type)) {
+      handlers[type]!!.forEach { it.invoke(payload) }
+    }
+  }
+
+  private fun checkEventHandlerMethodParameters(method: Method) {
+    if (method.parameterCount > 1) {
+      throw IllegalArgumentException("Method annotated with @Handler has more than 1 parameter")
+    }
+  }
+
+  private fun getEventHandlerMethods(component: Any): List<HandlerMethod> {
+    return component::class.java.declaredMethods
+        .filter { it.isAnnotationPresent(Handler::class.java) }
+        .map {
+          checkEventHandlerMethodParameters(it)
+          HandlerMethod(component, it, it.parameterTypes[0])
+        }
+  }
+
+  fun register(component: Any) {
+    val eventHandlers = getEventHandlerMethods(component)
+
+    eventHandlers.forEach { h ->
+      if (handlers.containsKey(h.parameterType)) {
+        handlers[h.parameterType]!!.add(h)
+      } else {
+        handlers[h.parameterType] = mutableListOf(h)
+      }
+    }
+  }
+}
