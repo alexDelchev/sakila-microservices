d10738dd3f8f843f415376e2c16172589a475d22
alexDelchev <a.r.delchev@outlook.com>
2020-05-09 14:12:39 +0300

    Convert CustomerEventEmitter to kotlin

---
+++ sakila-payment-service/src/main/java/com/example/sakila/module/customer/event/CustomerEventEmitter.kt
@@ -0,0 +1,54 @@
+package com.example.sakila.module.customer.event
+
+import com.example.sakila.event.bus.EventBus
+import com.example.sakila.event.bus.Handler
+import com.example.sakila.module.customer.event.model.CustomerCreatedEvent
+import com.example.sakila.module.customer.event.model.CustomerDeletedEvent
+import com.example.sakila.module.customer.event.model.CustomerUpdatedEvent
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.stereotype.Component
+import javax.annotation.PostConstruct
+
+@Component
+class CustomerEventEmitter @Autowired constructor(
+    @Qualifier("CustomerEventBus")
+    private val eventBus: EventBus,
+
+    private val kafkaTemplate: KafkaTemplate<String, String>
+) {
+
+  private val customerCreatedTopic = "sakila-payment-customer-created"
+
+  private val customerUpdatedTopic = "sakila-payment-customer-updated"
+
+  private val customerDeletedTopic = "sakila-payment-customer-deleted"
+
+  private val objectMapper = ObjectMapper().registerModule(KotlinModule())
+
+  @PostConstruct
+  private fun postConstruct() {
+    eventBus.register(component = this)
+  }
+
+  @Handler
+  fun onCustomerCreatedEvent(event: CustomerCreatedEvent) {
+    val serializedMessage = objectMapper.writeValueAsString(event)
+    kafkaTemplate.send(customerCreatedTopic, serializedMessage)
+  }
+
+  @Handler
+  fun onCustomerUpdatedEvent(event: CustomerUpdatedEvent) {
+    val serializedMessage = objectMapper.writeValueAsString(event)
+    kafkaTemplate.send(customerUpdatedTopic, serializedMessage)
+  }
+
+  @Handler
+  fun onCustomerDeletedEvent(event: CustomerDeletedEvent) {
+    val serializedMessage = objectMapper.writeValueAsString(event)
+    kafkaTemplate.send(customerDeletedTopic, serializedMessage)
+  }
+}
