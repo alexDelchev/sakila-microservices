0c00eba58dab07a1dbcab3a6e430ca78605d0356
alexDelchev <a.r.delchev@outlook.com>
2020-05-12 21:03:14 +0300

    Print to error log instead of stdout

--- sakila-store-read-service/src/main/java/com/example/sakila/config/TaskSchedulerErrorHandler.java
+++ sakila-store-read-service/src/main/java/com/example/sakila/config/TaskSchedulerErrorHandler.java
@@ -1,11 +1,15 @@
 package com.example.sakila.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.util.ErrorHandler;
 
 public class TaskSchedulerErrorHandler implements ErrorHandler {
 
+  private final Logger log = LoggerFactory.getLogger(TaskSchedulerErrorHandler.class);
+
   @Override
   public void handleError(Throwable throwable) {
-    throwable.printStackTrace();
+    log.error("Exception whil executing scheduled task", throwable);
   }
 }
