1ab96a6152b97ae09764e2d6ba37a4c33d7c8ac6
alexDelchev <a.r.delchev@outlook.com>
2020-05-12 08:33:44 +0300

    Remove this leakage from constructor
    This should not be exposed during construction. EventBus registration is moved to a @PostConstruct method

--- sakila-address-service/src/main/java/com/example/sakila/module/address/event/AddressEventPublisher.groovy
+++ sakila-address-service/src/main/java/com/example/sakila/module/address/event/AddressEventPublisher.groovy
@@ -13,6 +13,8 @@ import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Component
 
+import javax.annotation.PostConstruct
+
 @Slf4j
 @Component
 class AddressEventPublisher {
@@ -33,8 +35,11 @@ class AddressEventPublisher {
   AddressEventPublisher(@Qualifier('AddressEventBus') EventBus eventBus, KafkaTemplate kafkaTemplate) {
     this.eventBus = eventBus
     this.kafkaTemplate = kafkaTemplate
+  }
 
-    this.eventBus.register(this)
+  @PostConstruct
+  private void postConstruct() {
+    eventBus.register(this)
   }
 
   @Handler
