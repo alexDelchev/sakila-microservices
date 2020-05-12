6ccc2a16496a4c34800c648464ee060dd82b8e9f
alexDelchev <a.r.delchev@outlook.com>
2020-05-12 21:04:33 +0300

    Log lifecycle phases of Curator Framework

--- sakila-store-read-service/src/main/java/com/example/sakila/config/ZooKeeperLifeCycle.java
+++ sakila-store-read-service/src/main/java/com/example/sakila/config/ZooKeeperLifeCycle.java
@@ -1,6 +1,8 @@
 package com.example.sakila.config;
 
 import org.apache.curator.framework.CuratorFramework;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -9,6 +11,8 @@ import javax.annotation.PreDestroy;
 
 @Component
 public class ZooKeeperLifeCycle {
+
+  private final Logger log = LoggerFactory.getLogger(ZooKeeperLifeCycle.class);
 
   private final CuratorFramework curatorFramework;
 
@@ -19,11 +23,13 @@ public class ZooKeeperLifeCycle {
 
   @PostConstruct
   public void init() {
+    log.info("Starting Curator Framework");
     curatorFramework.start();
   }
 
   @PreDestroy
   public void destroy() {
+    log.info("Stopping Curator Framework");
     curatorFramework.close();
   }
 }
