f7c5f1dc6081f5042b3b52286e7db85d263d2e0d
alexDelchev <a.r.delchev@outlook.com>
2020-05-12 21:18:08 +0300

    Log write operations

--- sakila-store-read-service/src/main/java/com/example/sakila/module/staff/StaffService.java
+++ sakila-store-read-service/src/main/java/com/example/sakila/module/staff/StaffService.java
@@ -3,6 +3,8 @@ package com.example.sakila.module.staff;
 import com.example.sakila.exception.DataConflictException;
 import com.example.sakila.exception.NotFoundException;
 import com.example.sakila.module.staff.repository.StaffRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -11,6 +13,8 @@ import java.util.List;
 
 @Service
 public class StaffService {
+
+  private final Logger log = LoggerFactory.getLogger(StaffService.class);
 
   private final StaffRepository staffRepository;
 
@@ -30,12 +34,14 @@ public class StaffService {
   }
 
   public Staff createStaff(Staff staff) {
+    log.info("Creating Staff (ID: {})", staff.getId());
     return staffRepository.insertStaff(staff);
   }
 
   public Staff updateStaff(Long id, Staff source) {
     Staff target = getStaffById(id);
-    if (target == null) throw new NotFoundException("Staff for ID " + id + " does not exist");
+    if (target == null) throw new NotFoundException(String.format("Staff for ID %d does not exist", id));
+    log.info("Updating Staff (ID: {})", id);
 
     target.setFirstName(source.getFirstName());
     target.setLastName(source.getLastName());
@@ -52,7 +58,8 @@ public class StaffService {
 
   public void deleteStaff(Long id) {
     Staff staff = getStaffById(id);
-    if (staff == null) throw new NotFoundException("Staff for ID " + id + " does not exist");
+    if (staff == null) throw new NotFoundException(String.format("Staff for ID %d does not exist", id));
+    log.info("Deleting Staff (ID: {})", id);
 
     try {
       staffRepository.deleteStaff(staff);
