45594b66b926f22685e799aef18cdec7deb0f0a5
alexDelchev <a.r.delchev@outlook.com>
2020-05-16 16:07:47 +0300

    Move zookeeper host value to properties files

--- sakila-store-read-service/src/main/java/com/example/sakila/config/ZookeeperConfig.java
+++ sakila-store-read-service/src/main/java/com/example/sakila/config/ZookeeperConfig.java
@@ -3,22 +3,24 @@ package com.example.sakila.config;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ZookeeperConfig {
 
-  private static final String HOST = "localhost:2181";
-
   private static final int TIMEOUT = 10_000;
+
+  @Value("${zookeeper.url}")
+  private String host;
 
   @Bean
   public CuratorFramework curatorFramework() {
     return CuratorFrameworkFactory
         .builder()
         .retryPolicy(new ExponentialBackoffRetry(1000, 3))
-        .connectString(HOST)
+        .connectString(host)
         .connectionTimeoutMs(TIMEOUT)
         .sessionTimeoutMs(TIMEOUT)
         .build();
